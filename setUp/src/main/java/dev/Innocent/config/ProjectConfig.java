f13478bd12412aa4a0974e55ac7c6e74ca3dc97c
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-11 17:26:41 +0100

    Maven setUp

--- setUp/src/main/java/dev/Innocent/config/ProjectConfig.java
+++ setUp/src/main/java/dev/Innocent/config/ProjectConfig.java
@@ -13,12 +13,25 @@ to be used in the application.
 
 @Configuration
 public class ProjectConfig {
+
+    /*
+@Bean annotation, which lets Spring know that it needs to call
+this method when it initializes its context and adds the returned
+value to the context.
+* */
     @Bean
     Vehicle vehicle(){
         var veh = new Vehicle();
         veh.setName("Audi 8");
         return veh;
     }
+
+    /*
+The method names usually follow verbs notation.But for methods
+which we will use to create beans, can use nouns as names.
+This will be a good practise as the method names will become
+bean names as well in the context.
+* */
     @Bean
     String hello(){
         return "Hello World";
