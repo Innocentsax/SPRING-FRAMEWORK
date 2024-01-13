feff385fe39fa40db3824d54a65c7ac6256275f6
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-13 19:30:54 +0100

    Maven setUp4

--- setUp4/src/main/java/dev/Innocent/main/setUp4.java
+++ setUp4/src/main/java/dev/Innocent/main/setUp4.java
@@ -4,15 +4,13 @@ import dev.Innocent.beans.Vehicle;
 import dev.Innocent.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-public class setUp3 {
+public class setUp4 {
 
     public static void main(String[] args) {
 
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-
-        Vehicle veh1 = context.getBean("audiVehicle",Vehicle.class);
-        System.out.println("Primary Vehicle name from Spring Context is: " + veh1.getName());
-
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Primary Vehicle name from Spring Context is: " + vehicle.getName());
 
     }
 }
