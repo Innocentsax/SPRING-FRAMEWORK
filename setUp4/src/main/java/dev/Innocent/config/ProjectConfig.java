2d1875594922e45dc380ba9131cd721e329a8375
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-13 19:30:50 +0100

    Maven setUp4

--- setUp4/src/main/java/dev/Innocent/config/ProjectConfig.java
+++ setUp4/src/main/java/dev/Innocent/config/ProjectConfig.java
@@ -16,27 +16,34 @@ to be used in the application.
 public class ProjectConfig {
 
     /*
-@Bean annotation, which lets Spring know that it needs to call
-this method when it initializes its context and adds the returned
-value to the context.
-* */
-    @Bean
-    Vehicle vehicle1(){
+    @Bean annotation, which lets Spring know that it needs to call
+    this method when it initializes its context and adds the returned
+    value to the context.
+    * */
+    @Bean(name="audiVehicle")
+    Vehicle vehicle1() {
         var veh = new Vehicle();
-        veh.setName("Audi ");
+        veh.setName("Audi");
         return veh;
     }
 
-    @Bean
-    Vehicle vehicle2(){
+    @Bean(value="hondaVehicle")
+    Vehicle vehicle2() {
         var veh = new Vehicle();
         veh.setName("Honda");
         return veh;
     }
 
+    /*
+    When you have multiple beans of the same kind inside the Spring context,
+    you can make one of them primary by using @Primary annotation. Primary bean is
+    the one which Spring will choose if it has multiple options and you don’t specify a name.
+    In other words, it is the default bean that Spring Context will consider in case of
+    confusion due to multiple beans present of same type.
+    * */
     @Primary
-    @Bean("Ferrari")
-    Vehicle vehicle3(){
+    @Bean("ferrariVehicle")
+    Vehicle vehicle3() {
         var veh = new Vehicle();
         veh.setName("Ferrari");
         return veh;
