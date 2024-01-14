a8e2230b3689d30eb6fefcb75167269c6a2a77ed
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-14 08:59:19 +0100

    Building a Bank app

---
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/UdoBankApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent.udoBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdoBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdoBankApplication.class, args);
+	}
+
+}
---
+++ udoBank/udoBank/src/test/java/dev/Innocent/udoBank/UdoBankApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Innocent.udoBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdoBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
