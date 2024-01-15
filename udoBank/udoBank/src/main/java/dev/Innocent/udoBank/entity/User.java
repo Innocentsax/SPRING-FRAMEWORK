2d7f7ca9e4e727ffe42b4b255301ab5cd54442b4
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-15 17:54:46 +0100

    Building a Bank app

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/entity/User.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/entity/User.java
@@ -1,4 +1,15 @@
 package dev.Innocent.udoBank.entity;
 
+import java.math.BigDecimal;
+
 public class User {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String gender;
+    private String address;
+    private String stateOfOrigin;
+    private String accountNumber;
+    private BigDecimal accountBalance;
 }
