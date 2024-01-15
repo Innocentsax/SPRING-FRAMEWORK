4de8ac23c905b7d77d9396d17fd2fc1ae079ad6c
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-15 18:12:51 +0100

    Building a Bank app

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/entity/User.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/entity/User.java
@@ -1,8 +1,19 @@
 package dev.Innocent.udoBank.entity;
 
+import jakarta.persistence.Entity;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
 public class User {
     private Long id;
     private String firstName;
@@ -17,6 +28,8 @@ public class User {
     private String phoneNumber;
     private String alternativePhoneNumber;
     private String status;
+    @CreationTimestamp
     private LocalDateTime createdAt;
+    @UpdateTimestamp
     private LocalDateTime modifiedAt;
 }
