657b44900c918758e2ef3267fe4426bd120597aa
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-16 08:22:59 +0100

    Building a Bank app

---
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/UserServiceImpl.java
@@ -0,0 +1,12 @@
+package dev.Innocent.udoBank.service.ServiceImpl;
+
+import dev.Innocent.udoBank.DTO.BankResponse;
+import dev.Innocent.udoBank.DTO.UserRequest;
+import dev.Innocent.udoBank.service.UserService;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public BankResponse createAccount(UserRequest userRequest) {
+        return null;
+    }
+}
