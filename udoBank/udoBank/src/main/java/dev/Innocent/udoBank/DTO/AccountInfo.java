7cfb2206d726ecb816b0dab94f2d13f011265377
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-16 10:37:55 +0100

    Building a Bank app

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/DTO/AccountInfo.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/DTO/AccountInfo.java
@@ -5,12 +5,14 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
+
 @Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccountInfo {
     private String accountName;
-    private String accountBalance;
+    private BigDecimal accountBalance;
     private String accountNumber;
 }
