b81fc19a42b753e85bd008171c83e4dd6321df1f
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-16 10:38:04 +0100

    Building a Bank app

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/UserServiceImpl.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/UserServiceImpl.java
@@ -1,20 +1,37 @@
 package dev.Innocent.udoBank.service.ServiceImpl;
 
+import dev.Innocent.udoBank.DTO.AccountInfo;
 import dev.Innocent.udoBank.DTO.BankResponse;
 import dev.Innocent.udoBank.DTO.UserRequest;
 import dev.Innocent.udoBank.entity.User;
+import dev.Innocent.udoBank.repository.UserRepository;
 import dev.Innocent.udoBank.service.UserService;
 import dev.Innocent.udoBank.utils.AccountUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
-
+@Service
 public class UserServiceImpl implements UserService {
+    UserRepository userRepository;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
     @Override
     public BankResponse createAccount(UserRequest userRequest) {
         /**
          * Creating an account - saving a new user into the db
          * Check if a user already have an account
          */
+        if(userRepository.existsByEmail(userRequest.getEmail())){
+            return BankResponse.builder()
+                    .responseCode(AccountUtils.ACCOUNT_EXISTS_CODE)
+                    .responseMessage(AccountUtils.ACCOUNT_EXISTS_MESSAGE)
+                    .accountInfo(null)
+                    .build();
+        }
         User newUser = User.builder()
                 .firstName(userRequest.getFirstName())
                 .lastName(userRequest.getLastName())
@@ -27,6 +44,17 @@ public class UserServiceImpl implements UserService {
                 .phoneNumber(userRequest.getPhoneNumber())
                 .alternativePhoneNumber(userRequest.getAlternativePhoneNumber())
                 .status("ACTIVE")
+                .build();
+        User savedUser = userRepository.save(newUser);
+        return BankResponse.builder()
+                .responseCode(AccountUtils.ACCOUNT_CREATION_SUCCESS)
+                .responseMessage(AccountUtils.ACCOUNT_CREATION_MESSAGE)
+                .accountInfo(AccountInfo.builder()
+                        .accountBalance(savedUser.getAccountBalance())
+                        .accountNumber(savedUser.getAccountNumber())
+                        .accountName(savedUser.getFirstName() + " " + savedUser.getLastName() + " "
+                                + savedUser.getOtherName())
+                        .build())
                 .build();
     }
 }
