4e5e82237916ffb0b77a9fcc8c9bd498cec9c4c1
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-16 19:08:07 +0100

    Implementing Email Services

---
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/EmailServiceImpl.java
@@ -0,0 +1,37 @@
+package dev.Innocent.udoBank.service.ServiceImpl;
+
+import dev.Innocent.udoBank.DTO.EmailDetails;
+import dev.Innocent.udoBank.service.EmailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailServiceImpl implements EmailService {
+    private JavaMailSender javaMailSender;
+    @Value("${spring.mail.username}")
+    private String senderEmail;
+    @Autowired
+    public EmailServiceImpl(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Override
+    public void sendEmailAlert(EmailDetails emailDetails) {
+        try{
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(senderEmail);
+            mailMessage.setTo(emailDetails.getRecipient());
+            mailMessage.setText(emailDetails.getMessageBody());
+            mailMessage.setSubject(emailDetails.getSubject());
+
+            javaMailSender.send(mailMessage);
+            System.out.println("Mail sent successfully");
+        }catch (MailException e){
+            throw new RuntimeException(e);
+        }
+    }
+}
