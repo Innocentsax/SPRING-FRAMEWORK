eeb09e350f66c53d957d45d1e896735d04afa888
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-16 08:05:25 +0100

    Building a Bank app

---
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/DTO/UserRequest.java
@@ -0,0 +1,23 @@
+package dev.Innocent.udoBank.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRequest {
+    private String firstName;
+    private String lastName;
+    private String otherName;
+    private String gender;
+    private String address;
+    private String stateOfOrigin;
+    private String email;
+    private String phoneNumber;
+    private String alternativePhoneNumber;
+    private String status;
+}
