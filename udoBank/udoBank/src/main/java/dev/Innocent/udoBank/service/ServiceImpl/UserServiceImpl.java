8fce4eb0905433911dea495173d716ccfc9f430f
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-16 10:10:08 +0100

    Building a Bank app

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/UserServiceImpl.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/UserServiceImpl.java
@@ -2,11 +2,31 @@ package dev.Innocent.udoBank.service.ServiceImpl;
 
 import dev.Innocent.udoBank.DTO.BankResponse;
 import dev.Innocent.udoBank.DTO.UserRequest;
+import dev.Innocent.udoBank.entity.User;
 import dev.Innocent.udoBank.service.UserService;
+import dev.Innocent.udoBank.utils.AccountUtils;
+
+import java.math.BigDecimal;
 
 public class UserServiceImpl implements UserService {
     @Override
     public BankResponse createAccount(UserRequest userRequest) {
-        return null;
+        /**
+         * Creating an account - saving a new user into the db
+         * Check if a user already have an account
+         */
+        User newUser = User.builder()
+                .firstName(userRequest.getFirstName())
+                .lastName(userRequest.getLastName())
+                .otherName(userRequest.getOtherName())
+                .gender(userRequest.getGender())
+                .stateOfOrigin(userRequest.getStateOfOrigin())
+                .accountNumber(AccountUtils.generateAccountNumber())
+                .accountBalance(BigDecimal.ZERO)
+                .email(userRequest.getEmail())
+                .phoneNumber(userRequest.getPhoneNumber())
+                .alternativePhoneNumber(userRequest.getAlternativePhoneNumber())
+                .status("ACTIVE")
+                .build();
     }
 }
