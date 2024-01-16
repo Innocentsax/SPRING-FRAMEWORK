e0d1285dbf8bd6236dd54adac20dbb708e3e0507
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-16 19:07:53 +0100

    Implementing Email Services

---
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/DTO/EmailDetails.java
@@ -0,0 +1,17 @@
+package dev.Innocent.udoBank.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailDetails {
+    private String recipient;
+    private String messageBody;
+    private String subject;
+    private String attachment;
+}
