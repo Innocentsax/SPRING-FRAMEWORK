00fe18a0664e5744dba00d9a85520f147c756467
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-16 10:37:59 +0100

    Building a Bank app

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/utils/AccountUtils.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/utils/AccountUtils.java
@@ -6,6 +6,8 @@ public class AccountUtils {
 
     public static final String ACCOUNT_EXISTS_CODE = "001";
     public static final String ACCOUNT_EXISTS_MESSAGE = "This User already has an account created!";
+    public static final String ACCOUNT_CREATION_SUCCESS = "002";
+    public static final String ACCOUNT_CREATION_MESSAGE = "Account has been successfully created";
     public static String generateAccountNumber(){
         /**
          * 2024 + randomSixDigits
