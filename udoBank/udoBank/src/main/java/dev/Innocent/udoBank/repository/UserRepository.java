73483f10e034df29809f3278057d4dba6932807f
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-17 22:04:06 +0100

    Implementing AccountBalance and name Enquire

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/repository/UserRepository.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/repository/UserRepository.java
@@ -4,5 +4,7 @@ import dev.Innocent.udoBank.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    boolean existsByEmail(String email);
+    Boolean existsByEmail(String email);
+    Boolean existsByAccountNumber(String accountNumber);
+    User findByAccountNumber(String accountNumber);
 }
