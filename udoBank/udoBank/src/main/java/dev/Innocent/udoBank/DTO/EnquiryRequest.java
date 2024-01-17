e9220043eed97080e46e2cc87b99ed0f43b9b1fc
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-17 22:04:02 +0100

    Implementing AccountBalance and name Enquire

---
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/DTO/EnquiryRequest.java
@@ -0,0 +1,14 @@
+package dev.Innocent.udoBank.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class EnquiryRequest {
+    private String accountNumber;
+}
