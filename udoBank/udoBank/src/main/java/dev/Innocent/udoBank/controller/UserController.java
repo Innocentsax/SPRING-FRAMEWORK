18b12678d60190a366d46d54744ec8536949ce6a
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-18 11:50:55 +0100

    Implementing credit and Debit Account

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/controller/UserController.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/controller/UserController.java
@@ -32,4 +32,8 @@ public class UserController {
     public BankResponse creditAccount(@RequestBody CreditDebitRequest request){
         return userService.creditAccount(request);
     }
+    @PostMapping("/debit")
+    public BankResponse debitAccount(@RequestBody CreditDebitRequest request){
+        return userService.debitAccount(request);
+    }
 }
