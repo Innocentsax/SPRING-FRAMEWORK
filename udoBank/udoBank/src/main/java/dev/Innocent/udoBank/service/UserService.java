f1121e81f319e85c5b7685c10015d948c43ed404
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-18 11:51:01 +0100

    Implementing credit and Debit Account

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/UserService.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/UserService.java
@@ -10,4 +10,5 @@ public interface UserService {
     BankResponse balanceEnquiry(EnquiryRequest enquiryRequest);
     String nameEnquiry(EnquiryRequest enquiryRequest);
     BankResponse creditAccount(CreditDebitRequest request);
+    BankResponse debitAccount(CreditDebitRequest request);
 }
