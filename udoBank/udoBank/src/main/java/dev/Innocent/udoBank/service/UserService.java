3dcc0e2846c4090eed5c3dab35117d909f4b55d0
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-18 10:19:18 +0100

    Implementing AccountBalance and name Enquire

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/UserService.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/UserService.java
@@ -1,6 +1,7 @@
 package dev.Innocent.udoBank.service;
 
 import dev.Innocent.udoBank.DTO.BankResponse;
+import dev.Innocent.udoBank.DTO.CreditDebitRequest;
 import dev.Innocent.udoBank.DTO.EnquiryRequest;
 import dev.Innocent.udoBank.DTO.UserRequest;
 
@@ -8,4 +9,5 @@ public interface UserService {
     BankResponse createAccount(UserRequest userRequest);
     BankResponse balanceEnquiry(EnquiryRequest enquiryRequest);
     String nameEnquiry(EnquiryRequest enquiryRequest);
+    BankResponse creditAccount(CreditDebitRequest request);
 }
