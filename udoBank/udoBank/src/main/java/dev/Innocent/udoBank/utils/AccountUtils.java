a95fb248ce32c733367c0457107ca4f62ca150d6
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-18 11:50:48 +0100

    Implementing credit and Debit Account

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/utils/AccountUtils.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/utils/AccountUtils.java
@@ -14,6 +14,10 @@ public class AccountUtils {
     public static final String ACCOUNT_FOUND_SUCCESS_MESSAGE = "User Account Found";
     public static final String ACCOUNT_CREDITED_SUCCESS = "005";
     public static final String ACCOUNT_CREDITED_SUCCESS_MESSAGE = "You account has been credited successfully";
+    public static final String INSUFFICIENT_BALANCE_CODE = "006";
+    public static final String INSUFFICIENT_BALANCE_MESSAGE = "Insufficient Balance";
+    public static final String ACCOUNT_DEBITED_SUCCESS = "007";
+    public static final String ACCOUNT_DEBITED_MESSAGE = "Account has been successfully debited";
     public static String generateAccountNumber(){
         /**
          * 2024 + randomSixDigits
