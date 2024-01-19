50f6e5cb65e0ce2f81f1d20b2031c36f17f027e5
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-19 22:41:49 +0100

    Implementing Implementing Transfer

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/utils/AccountUtils.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/utils/AccountUtils.java
@@ -18,6 +18,8 @@ public class AccountUtils {
     public static final String INSUFFICIENT_BALANCE_MESSAGE = "Insufficient Balance";
     public static final String ACCOUNT_DEBITED_SUCCESS = "007";
     public static final String ACCOUNT_DEBITED_MESSAGE = "Account has been successfully debited";
+    public static final String TRANSFER_SUCCESSFUL_CODE = "008";
+    public static final String TRANSFER_SUCCESSFUL_MESSAGE = "Transfer Successful";
     public static String generateAccountNumber(){
         /**
          * 2024 + randomSixDigits
