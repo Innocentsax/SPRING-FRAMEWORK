19d18c13eab6ca413c725d9dd1773201d768a6a9
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-20 15:37:20 +0100

    Implementing Swagger ui documentation

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/UdoBankApplication.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/UdoBankApplication.java
@@ -1,9 +1,22 @@
 package dev.Innocent.udoBank;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Innocent Udo's Bank",
+				description = "Backend Rest APIs for UdoBank",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Innocent Udo"
+				)
+		)
+)
 public class UdoBankApplication {
 
 	public static void main(String[] args) {
