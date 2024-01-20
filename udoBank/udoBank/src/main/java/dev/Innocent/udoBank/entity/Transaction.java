67f12e9025546142e85610886bddd07d81513c16
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-20 18:38:43 +0100

    Implementing PDF feat

---
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/entity/Transaction.java
@@ -0,0 +1,23 @@
+package dev.Innocent.udoBank.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "transactions")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String transactionId;
+    private String transactionType;
+    private BigDecimal amount;
+    private String accountNumber;
+    private String status;
+}
