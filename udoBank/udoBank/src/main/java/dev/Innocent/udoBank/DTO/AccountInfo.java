22786406a0901894891d44c02c0400542467a404
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-20 17:36:31 +0100

    Implementing Swagger ui documentation

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/DTO/AccountInfo.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/DTO/AccountInfo.java
@@ -1,5 +1,6 @@
 package dev.Innocent.udoBank.DTO;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,16 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccountInfo {
+    @Schema(
+            name = "User Account name"
+    )
     private String accountName;
+    @Schema(
+            name = "User Account balance"
+    )
     private BigDecimal accountBalance;
+    @Schema(
+            name = "User Account number"
+    )
     private String accountNumber;
 }
