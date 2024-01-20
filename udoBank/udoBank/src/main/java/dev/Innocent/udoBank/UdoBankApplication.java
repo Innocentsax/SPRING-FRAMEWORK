e64d073e0a31539073814d83585405fa927bad6b
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-20 15:55:42 +0100

    Implementing Swagger ui documentation

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/UdoBankApplication.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/UdoBankApplication.java
@@ -1,8 +1,10 @@
 package dev.Innocent.udoBank;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,8 +15,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 				description = "Backend Rest APIs for UdoBank",
 				version = "v1.0",
 				contact = @Contact(
-						name = "Innocent Udo"
+						name = "Innocent Udo",
+						email = "innocentcharlesudo@gmail.com",
+						url = "https://github.com/Innocentsax"
+				),
+				license = @License(
+						name = "MIT license",
+						url = "https://github.com/Innocentsax"
 				)
+		),
+		externalDocs = @ExternalDocumentation(
+				description = "Backend Rest APIs for UdoBank",
+				url = "https://github.com/Innocentsax"
 		)
 )
 public class UdoBankApplication {
