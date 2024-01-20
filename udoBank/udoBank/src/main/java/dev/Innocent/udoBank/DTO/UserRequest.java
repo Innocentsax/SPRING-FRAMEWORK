76da5209e964b89e904da05012c387af8efd7c54
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-20 18:39:09 +0100

    Implementing PDF feat

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/DTO/UserRequest.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/DTO/UserRequest.java
@@ -4,11 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Data
-@Builder
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserRequest {
     private String firstName;
     private String lastName;
