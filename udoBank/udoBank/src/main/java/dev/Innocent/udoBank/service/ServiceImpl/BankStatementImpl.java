7ee4bfaf02855bf253968819f89f6d48b850a1e7
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-24 19:46:48 +0100

    Implementing Bank Statement Implementation

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/BankStatementImpl.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/BankStatementImpl.java
@@ -1,8 +1,10 @@
 package dev.Innocent.udoBank.service.ServiceImpl;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class BankStatementImpl {
     /**
      * Retrieve list of transactions within a date range given an account number
