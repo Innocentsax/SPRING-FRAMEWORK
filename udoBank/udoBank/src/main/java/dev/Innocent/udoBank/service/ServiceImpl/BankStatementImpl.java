63fe5e1dbef387add878b47b89c539bf2e0ba88d
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-25 15:52:46 +0100

    Implementing PDF feat

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/BankStatementImpl.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/BankStatementImpl.java
@@ -1,8 +1,11 @@
 package dev.Innocent.udoBank.service.ServiceImpl;
 
+import dev.Innocent.udoBank.entity.Transaction;
 import dev.Innocent.udoBank.repository.TransactionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 @AllArgsConstructor
@@ -13,4 +16,8 @@ public class BankStatementImpl {
      * Generate a pdf file of transactions
      * Send the file via email
      */
+    public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate){
+        List<Transaction> transactionList = transactionRepository.findAll().stream().filter(transaction ->
+                transaction.getAccountNumber().equals(accountNumber)).filter(transaction -> transaction.getCreatedAt())
+    }
 }
