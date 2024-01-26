c84062436b661d57713dd4671c0b01edb03b88f6
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-26 12:25:11 +0100

    Implementing PDF feat

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/BankStatementImpl.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/BankStatementImpl.java
@@ -5,6 +5,8 @@ import dev.Innocent.udoBank.repository.TransactionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @Component
@@ -17,7 +19,11 @@ public class BankStatementImpl {
      * Send the file via email
      */
     public List<Transaction> generateStatement(String accountNumber, String startDate, String endDate){
+        LocalDate start = LocalDate.parse(startDate, DateTimeFormatter.ISO_DATE);
+        LocalDate end = LocalDate.parse(endDate, DateTimeFormatter.ISO_DATE);
         List<Transaction> transactionList = transactionRepository.findAll().stream().filter(transaction ->
-                transaction.getAccountNumber().equals(accountNumber)).filter(transaction -> transaction.getCreatedAt())
+                transaction.getAccountNumber().equals(accountNumber)).filter(transaction ->
+                transaction.getCreatedAt().isEqual(start.atStartOfDay())).filter((transaction -> transaction.getCreatedAt().isEqual(end.atStartOfDay()))).toList();
+        return transactionList;
     }
 }
