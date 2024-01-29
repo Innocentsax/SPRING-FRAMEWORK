6783a003c05ee3e3e9dbcad864bfa7528f60ad0d
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-29 15:16:04 +0100

    Implementing PDF feat

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/EmailService.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/EmailService.java
@@ -4,4 +4,5 @@ import dev.Innocent.udoBank.DTO.EmailDetails;
 
 public interface EmailService {
     void sendEmailAlert(EmailDetails emailDetails);
+    void sendEmailWithAttachment(EmailDetails emailDetails);
 }
