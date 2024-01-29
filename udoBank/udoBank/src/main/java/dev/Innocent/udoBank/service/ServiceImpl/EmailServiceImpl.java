6688d0359d90525481e54a038581bd0405a871a9
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-29 15:58:57 +0100

    Implementing PDF feat

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/EmailServiceImpl.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/EmailServiceImpl.java
@@ -4,15 +4,21 @@ import dev.Innocent.udoBank.DTO.EmailDetails;
 import dev.Innocent.udoBank.service.EmailService;
 import jakarta.mail.MessagingException;
 import jakarta.mail.internet.MimeMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.MailException;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.util.Objects;
+
 @Service
+@Slf4j
 public class EmailServiceImpl implements EmailService {
     private JavaMailSender javaMailSender;
     @Value("${spring.mail.username}")
@@ -48,6 +54,10 @@ public class EmailServiceImpl implements EmailService {
             mimeMessageHelper.setTo(emailDetails.getRecipient());
             mimeMessageHelper.setText(emailDetails.getMessageBody());
             mimeMessageHelper.setSubject(emailDetails.getSubject());
+
+            FileSystemResource file = new FileSystemResource(new File(emailDetails.getAttachment()));
+            mimeMessageHelper.addAttachment(Objects.requireNonNull(file.getFilename()), file);
+            javaMailSender.send(mimeMessage);
         } catch (MessagingException e) {
             throw new RuntimeException(e);
         }
