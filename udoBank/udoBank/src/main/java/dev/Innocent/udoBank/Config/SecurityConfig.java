5788384113d801d5d475d96770d1534e3392692c
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-31 22:02:15 +0100

    Implementing PDF feat

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/Config/SecurityConfig.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/Config/SecurityConfig.java
@@ -2,10 +2,14 @@ package dev.Innocent.udoBank.Config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
@@ -14,5 +18,13 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize ->
+                        authorize.requestMatchers(HttpMethod.POST, "/api/user").permitAll()
+                                .anyRequest().authenticated());
     }
 }
