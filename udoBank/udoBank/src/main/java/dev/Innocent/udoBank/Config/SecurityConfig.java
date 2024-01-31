fa6806d3cc943d1c9ba475b881b8ffe5273cd5b0
Innocent Udo <innocentcharlesudo@gmail.com>
2024-01-31 20:09:04 +0100

    Implementing PDF feat

---
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/Config/SecurityConfig.java
@@ -0,0 +1,18 @@
+package dev.Innocent.udoBank.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
