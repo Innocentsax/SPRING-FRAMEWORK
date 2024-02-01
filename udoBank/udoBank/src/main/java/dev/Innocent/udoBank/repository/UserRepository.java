814dc506ad67126cdada2f85cdec0ccaa51fd2cf
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-02 00:03:49 +0100

    Implementing JWT token

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/repository/UserRepository.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/repository/UserRepository.java
@@ -3,8 +3,11 @@ package dev.Innocent.udoBank.repository;
 import dev.Innocent.udoBank.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByEmail(String email);
     Boolean existsByAccountNumber(String accountNumber);
     User findByAccountNumber(String accountNumber);
+    Optional<User> findByEmail(String email);
 }
