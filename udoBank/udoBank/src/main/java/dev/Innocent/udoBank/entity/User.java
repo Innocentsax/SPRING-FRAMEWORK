3a088d1e440d42864c1877bf1dc94c80f1507ed3
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-02 00:03:45 +0100

    Implementing JWT token

--- udoBank/udoBank/src/main/java/dev/Innocent/udoBank/entity/User.java
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/entity/User.java
@@ -1,12 +1,18 @@
 package dev.Innocent.udoBank.entity;
 
+import dev.Innocent.udoBank.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
 
 @Getter
 @Setter
@@ -15,7 +21,7 @@ import java.time.LocalDateTime;
 @Builder
 @Entity
 @Table(name = "users")
-public class User {
+public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -28,6 +34,7 @@ public class User {
     private String accountNumber;
     private BigDecimal accountBalance;
     private String email;
+    private Role role;
     private String password;
     private String phoneNumber;
     private String alternativePhoneNumber;
@@ -36,4 +43,39 @@ public class User {
     private LocalDateTime createdAt;
     @UpdateTimestamp
     private LocalDateTime modifiedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
