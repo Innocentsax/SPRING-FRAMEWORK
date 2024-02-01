72af8aa270dcc117dbfb6fcd5582bbbbd28e1a5c
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-01 21:39:10 +0100

    Implementing JWT token

---
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/service/ServiceImpl/CustomUserDetailsService.java
@@ -0,0 +1,12 @@
+package dev.Innocent.udoBank.service.ServiceImpl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
