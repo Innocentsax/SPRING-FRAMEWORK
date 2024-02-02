f1f4693e5620db5d49c137c5b9b16545b0e2143b
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-02 21:56:37 +0100

    Implementing JWT token

---
+++ udoBank/udoBank/src/main/java/dev/Innocent/udoBank/DTO/LoginDTO.java
@@ -0,0 +1,13 @@
+package dev.Innocent.udoBank.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDTO {
+    private String email;
+    private String password;
+}
