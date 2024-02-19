6af541b5cf8ba590387f1011eb7918ea39509377
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-19 09:48:41 +0100

    Authentication system

---
+++ Authentication System/Authentication-System/src/main/java/dev/Innocent/AuthenticationSystem/entity/User.java
@@ -0,0 +1,28 @@
+package dev.Innocent.AuthenticationSystem.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+@Builder
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+    @UpdateTimestamp
+    private LocalDateTime modifiedAt;
+}
