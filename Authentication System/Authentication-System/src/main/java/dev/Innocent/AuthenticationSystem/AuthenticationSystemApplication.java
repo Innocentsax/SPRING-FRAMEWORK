7d5d272f71ecc3d1c44c2860b879abb780ea331d
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-20 21:41:01 +0100

    Authentication system

--- Authentication System/Authentication-System/src/main/java/dev/Innocent/AuthenticationSystem/AuthenticationSystemApplication.java
+++ Authentication System/Authentication-System/src/main/java/dev/Innocent/AuthenticationSystem/AuthenticationSystemApplication.java
@@ -1,10 +1,17 @@
 package dev.Innocent.AuthenticationSystem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AuthenticationSystemApplication {
+
+	@Bean
+	public ModelMapper modelMapper(){
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticationSystemApplication.class, args);
