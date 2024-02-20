d42064503c8ba219d2e0cd95e1da3e171552c0fa
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-20 21:41:06 +0100

    Authentication system

---
+++ Authentication System/Authentication-System/src/main/java/dev/Innocent/AuthenticationSystem/DTO/EmailDetails.java
@@ -0,0 +1,16 @@
+package dev.Innocent.AuthenticationSystem.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class EmailDetails {
+    private String recipient;
+    private String subject;
+    private String messageBody;
+}
