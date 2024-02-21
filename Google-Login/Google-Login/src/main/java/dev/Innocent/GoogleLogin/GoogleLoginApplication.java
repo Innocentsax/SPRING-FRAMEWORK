74a4338ef6929cf2ee90865d52e0c5cbaa28b7e7
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-21 14:46:43 +0100

    Social Login

---
+++ Google-Login/Google-Login/src/main/java/dev/Innocent/GoogleLogin/GoogleLoginApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent.GoogleLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleLoginApplication.class, args);
+	}
+
+}
---
+++ Google-Login/Google-Login/src/test/java/dev/Innocent/GoogleLogin/GoogleLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Innocent.GoogleLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
