78b8f2c2de257e28272b8106dec43ff41a26bb00
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-23 00:27:19 +0100

    Jwt

---
+++ AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/controller/UserController.java
@@ -0,0 +1,16 @@
+package dev.Innocent.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user")
+@CrossOrigin("*")
+public class UserController {
+    @GetMapping
+    public String helloUserController(){
+        return "User access level";
+    }
+}
