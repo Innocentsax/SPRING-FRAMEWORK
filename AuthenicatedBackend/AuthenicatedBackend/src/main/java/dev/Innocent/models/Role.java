2a78d089279fe3778fd9fe5433444d941171a343
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-25 10:25:29 +0100

    Jwt

--- AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/models/Role.java
+++ AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/models/Role.java
@@ -1,22 +1,47 @@
 package dev.Innocent.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Setter
-@Getter
-@Table(name = "roles")
+@Table(name="roles")
 public class Role implements GrantedAuthority {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy=GenerationType.AUTO)
     @Column(name="role_id")
     private Integer roleId;
+
     private String authority;
+
+    public Role(){
+        super();
+    }
+
+    public Role(String authority){
+        this.authority = authority;
+    }
+
+    public Role(Integer roleId, String authority){
+        this.roleId = roleId;
+        this.authority = authority;
+    }
+
+    @Override
+    public String getAuthority() {
+        // TODO Auto-generated method stub
+        return this.authority;
+    }
+
+    public void setAuthority(String authority){
+        this.authority = authority;
+    }
+
+    public Integer getRoleId(){
+        return this.roleId;
+    }
+
+    public void setRoleId(Integer roleId){
+        this.roleId = roleId;
+    }
 }
