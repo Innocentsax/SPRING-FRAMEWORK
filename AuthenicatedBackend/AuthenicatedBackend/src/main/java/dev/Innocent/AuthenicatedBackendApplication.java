9af07a4a6ad1ed92437886525eed5cb1779f6db9
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-25 10:25:24 +0100

    Jwt

--- AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/AuthenicatedBackendApplication.java
+++ AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/AuthenicatedBackendApplication.java
@@ -1,13 +1,38 @@
 package dev.Innocent;
 
+import dev.Innocent.models.AppUser;
+import dev.Innocent.models.Role;
+import dev.Innocent.repository.RoleRepository;
+import dev.Innocent.repository.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.HashSet;
+import java.util.Set;
 
 @SpringBootApplication
 public class AuthenicatedBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenicatedBackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder){
+		return args -> {
+			if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("USER"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			AppUser admin = new AppUser(1, "admin", passwordEncoder.encode("password"), roles);
+			userRepository.save(admin);
+		};
 	}
 
 }
