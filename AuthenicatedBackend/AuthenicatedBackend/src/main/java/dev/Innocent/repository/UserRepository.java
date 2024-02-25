01c7d5d850eed4e220274a5cdb749098fb27a14a
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-25 10:25:32 +0100

    Jwt

--- AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/repository/UserRepository.java
+++ AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/repository/UserRepository.java
@@ -7,5 +7,5 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<AppUser, Integer> {
-    Optional<AppUser> findByEmail(String email);
+    Optional<AppUser> findByUsername(String username);
 }
