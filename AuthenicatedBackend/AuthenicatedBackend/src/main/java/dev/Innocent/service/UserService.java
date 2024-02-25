2809cd06d0be0f435e76b84df7ed3c852b5f20a0
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-25 10:25:43 +0100

    Jwt

--- AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/service/UserService.java
+++ AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/service/UserService.java
@@ -1,27 +1,21 @@
 package dev.Innocent.service;
 
-import dev.Innocent.models.AppUser;
-import dev.Innocent.models.Role;
+import dev.Innocent.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.HashSet;
-import java.util.Set;
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private PasswordEncoder encoder;
+    @Autowired
+    private UserRepository userRepository;
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         System.out.println("In the user details service");
-        if(!username.equals("Ethan")) throw new UsernameNotFoundException("Not Ethan");
-        Set<Role> roles = new HashSet<>();
-        roles.add(new Role(1,"USER"));
-
-        return new AppUser(1, "Ethan", encoder.encode("password"), roles);
+        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("user is not valid"));
     }
 }
