d653026cad4a0872e53fce9ba49cf3ec95cd6c7f
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-25 10:25:21 +0100

    Jwt

--- AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/models/AppUser.java
+++ AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/models/AppUser.java
@@ -1,69 +1,106 @@
 package dev.Innocent.models;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@AllArgsConstructor
-@NoArgsConstructor
-@Setter
-@Getter
-@Table(name = "user")
-public class AppUser implements UserDetails {
+@Table(name="users")
+public class AppUser implements UserDetails{
+
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name="user_id")
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Integer userId;
+    @Column(unique=true)
     private String username;
     private String password;
 
-    @ManyToMany(fetch = FetchType.EAGER)
+    @ManyToMany(fetch=FetchType.EAGER)
     @JoinTable(
             name="user_role_junction",
             joinColumns = {@JoinColumn(name="user_id")},
             inverseJoinColumns = {@JoinColumn(name="role_id")}
     )
     private Set<Role> authorities;
+
+    public AppUser() {
+        super();
+        authorities = new HashSet<>();
+    }
+
+
+    public AppUser(Integer userId, String username, String password, Set<Role> authorities) {
+        super();
+        this.userId = userId;
+        this.username = username;
+        this.password = password;
+        this.authorities = authorities;
+    }
+
+    public Integer getUserId() {
+        return this.userId;
+    }
+
+    public void setId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public void setAuthorities(Set<Role> authorities) {
+        this.authorities = authorities;
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
+        // TODO Auto-generated method stub
         return this.authorities;
     }
 
     @Override
     public String getPassword() {
+        // TODO Auto-generated method stub
         return this.password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     @Override
     public String getUsername() {
+        // TODO Auto-generated method stub
         return this.username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    /* If you want account locking capabilities create variables and ways to set them for the methods below */
     @Override
     public boolean isAccountNonExpired() {
+        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
+        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
+        // TODO Auto-generated method stub
         return true;
     }
 
     @Override
     public boolean isEnabled() {
+        // TODO Auto-generated method stub
         return true;
     }
 }
