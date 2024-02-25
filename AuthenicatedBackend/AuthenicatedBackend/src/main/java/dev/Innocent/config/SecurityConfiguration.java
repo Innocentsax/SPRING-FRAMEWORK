06f07dd907a679d0eed9cb11ea40eeb0dbecda5b
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-25 11:33:38 +0100

    Jwt

--- AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/config/SecurityConfiguration.java
+++ AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/config/SecurityConfiguration.java
@@ -27,7 +27,10 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated())
+                .authorizeHttpRequests(auth -> {
+                    auth.requestMatchers("/auth/**").permitAll();
+                    auth.anyRequest().authenticated();
+                })
                 .httpBasic().and()
                 .build();
     }
