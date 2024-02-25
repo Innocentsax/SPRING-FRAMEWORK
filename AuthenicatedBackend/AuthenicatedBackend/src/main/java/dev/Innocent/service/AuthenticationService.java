b42863b461e3c73aee64a8b116702814b665ebb3
Innocent Udo <innocentcharlesudo@gmail.com>
2024-02-25 11:33:33 +0100

    Jwt

--- AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/service/AuthenticationService.java
+++ AuthenicatedBackend/AuthenicatedBackend/src/main/java/dev/Innocent/service/AuthenticationService.java
@@ -9,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Service
 @Transactional
 public class AuthenticationService {
@@ -26,6 +29,9 @@ public class AuthenticationService {
 
     public AppUser registerUser(String username, String password){
         String encodedPassword = passwordEncoder.encode(password);
-        Role userRole = roleRepository.findByAuthority()
+        Role userRole = roleRepository.findByAuthority("USER").get();
+        Set<Role> authorities = new HashSet<>();
+        authorities.add(userRole);
+        return userRepository.save(new AppUser(0, username, encodedPassword, authorities));
     }
 }
