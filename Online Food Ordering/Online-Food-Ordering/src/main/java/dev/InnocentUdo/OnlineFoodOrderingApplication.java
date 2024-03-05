d6378d04ea1251dd9c42329449707bed55bf5b1d
Innocent Udo <innocentcharlesudo@gmail.com>
2024-03-05 22:30:10 +0100

    Online Food Ordering System

---
+++ Online Food Ordering/Online-Food-Ordering/src/main/java/dev/InnocentUdo/OnlineFoodOrderingApplication.java
@@ -0,0 +1,13 @@
+package dev.InnocentUdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineFoodOrderingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineFoodOrderingApplication.class, args);
+	}
+
+}
