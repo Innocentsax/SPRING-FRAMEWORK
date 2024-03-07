5edd50a6f03878dd28efd9c220eafaab54bfb7bd
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-07 19:34:40 +0100

    Online shopping

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/User.java
@@ -0,0 +1,14 @@
+package dev.InnocentUdo.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+}
