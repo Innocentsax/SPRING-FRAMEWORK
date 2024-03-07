e3e54b543e8ddec50386e00e23c73c4a7932f866
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-07 23:03:32 +0100

    Online shopping

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/Address.java
@@ -0,0 +1,13 @@
+package dev.InnocentUdo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
