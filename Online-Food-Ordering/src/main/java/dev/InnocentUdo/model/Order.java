c7c6c446bf503c10932f622d0ab60bc1d331b0e4
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-07 23:03:59 +0100

    Online shopping

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/Order.java
@@ -0,0 +1,37 @@
+package dev.InnocentUdo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @ManyToOne
+    private User customer;
+    private Long totalAmount;
+    private String orderStatus;
+    private Date createdAt;
+    private int totalItem;
+    private int totalPrice;
+
+    @ManyToOne
+    private Address deliveryAddress;
+
+    @OneToMany
+    private List<OrderItem> items;
+
+    @JsonIgnore
+    @ManyToOne
+    private Restaurant restaurant;
+}
