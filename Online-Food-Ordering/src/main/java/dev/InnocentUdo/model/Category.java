3ccb00010ea0dc7e0fd773f5e3785a65a287fcb9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-07 23:04:03 +0100

    Online shopping

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/Category.java
@@ -0,0 +1,19 @@
+package dev.InnocentUdo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+}
