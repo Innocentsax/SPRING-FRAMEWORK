b2c3f5b902227d01dccba6e6460b6918f7081a8d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-07 23:03:43 +0100

    Online shopping

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/User.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/User.java
@@ -1,14 +1,36 @@
 package dev.InnocentUdo.model;
 
-import jakarta.persistence.Entity;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import dev.InnocentUdo.DTO.RestaurantDTO;
+import dev.InnocentUdo.enums.USER_ROLE;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+    private USER_ROLE role;
 
+    @JsonIgnore
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    private List<RestaurantDTO> favorites = new ArrayList<>();
+
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
 }
