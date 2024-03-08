6020674d35f4300b3c0b8350d3376c0d70c15f47
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 18:51:06 +0100

    Ft | Jwt impl, login and sign

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/CustomUserDetails.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/CustomUserDetails.java
@@ -22,7 +22,7 @@ public class CustomUserDetails implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         User user = userRepository.findByEmail(username);
-        if(user != null){
+        if(user == null){
             throw new UsernameNotFoundException("User not found with email " + username);
         }
         USER_ROLE role = user.getRole();
