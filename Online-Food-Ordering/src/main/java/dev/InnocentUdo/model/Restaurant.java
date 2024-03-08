8ac6d4b0d68d0b2f8653912a2cfcf702dd7b0dc7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 11:48:13 +0100

    Ft | Entity Creation and design

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/Restaurant.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/Restaurant.java
@@ -1,12 +1,14 @@
 package dev.InnocentUdo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -16,4 +18,31 @@ public class Restaurant {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String name;
+
+    @OneToOne
+    private User owner;
+    private String description;
+    private String cuisineType;
+    private String openingHours;
+
+    @OneToOne
+    private Address address;
+
+    @Embedded
+    private ContactInformation contactinformation;
+
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Order> orders = new ArrayList<>();
+
+    @ElementCollection
+    @Column(length = 1000)
+    private List<String> images;
+
+    private LocalDateTime registrationDate;
+    private boolean open;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL)
+    private List<Food> food = new ArrayList<>();
 }
