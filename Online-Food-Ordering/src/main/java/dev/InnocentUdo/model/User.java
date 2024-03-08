c953f0bd0bc824701bd2ef2f29b6f5edf29e649e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 14:15:28 +0100

    Ft | Jwt impl

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/User.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/User.java
@@ -22,7 +22,7 @@ public class User {
     private String fullName;
     private String email;
     private String password;
-    private USER_ROLE role;
+    private USER_ROLE role = USER_ROLE.ROLE_CUSTOMER;
 
     @JsonIgnore
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "customer")
