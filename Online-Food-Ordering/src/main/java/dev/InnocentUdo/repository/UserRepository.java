1c67cbfec7c4bc22f5ec3c2f52f3d45685e4a7f3
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 14:15:25 +0100

    Ft | Jwt impl

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package dev.InnocentUdo.repository;
+
+import dev.InnocentUdo.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String username);
+}
