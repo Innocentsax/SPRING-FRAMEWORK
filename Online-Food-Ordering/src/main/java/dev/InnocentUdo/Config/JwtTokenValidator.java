0230c70748c236c3309f388404512ba51ac99ebf
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 14:15:23 +0100

    Ft | Jwt impl

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/Config/JwtTokenValidator.java
@@ -0,0 +1,50 @@
+package dev.InnocentUdo.Config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.crypto.SecretKey;
+import java.io.IOException;
+import java.util.List;
+
+public class JwtTokenValidator extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        String jwt = request.getHeader(JwtConstant.JWT_HEADER);
+
+        // Bearer token
+        // 1234567
+        if(jwt != null){
+            jwt = jwt.substring(7);
+            try{
+                SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+                Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+
+                String email = String.valueOf(claims.get("email"));
+                String authorities = String.valueOf((claims.get("authorities")));
+
+                // authorities collected in String format then convert to GrantedAuthority (ROLE_CUSTOMER, ROLE_ADMIN)
+                List<GrantedAuthority> auth = AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+                Authentication authentication = new UsernamePasswordAuthenticationToken(email, null, auth);
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }catch (Exception e){
+                throw new BadCredentialsException("Invalid token");
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
