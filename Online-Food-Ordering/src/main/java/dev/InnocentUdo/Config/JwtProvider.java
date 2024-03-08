b7d9577ff04342d02d949198d620cf79b7d48a5e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 15:04:03 +0100

    Ft | Jwt impl

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/Config/JwtProvider.java
@@ -0,0 +1,50 @@
+package dev.InnocentUdo.Config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class JwtProvider {
+    private SecretKey key = Keys.hmacShaKeyFor(JwtConstant.SECRET_KEY.getBytes());
+
+    public String generateToken(Authentication auth){
+        Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
+        String roles = populateAuthorities(authorities);
+
+        String jwt = Jwts.builder().setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 86400000))
+                .claim("email",auth.getName())
+                .claim("authorities",roles)
+                .signWith(key)
+                .compact();
+
+        return jwt;
+    }
+
+    public String getEmailFromJwtToken(String jwt){
+        jwt = jwt.substring(7);
+        Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+
+        String email = String.valueOf(claims.get("email"));
+        return email;
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+        Set<String> auths = new HashSet<>();
+
+        for(GrantedAuthority authority : authorities){
+            auths.add(authority.getAuthority());
+        }
+        return String.join(",",auths);
+    }
+}
