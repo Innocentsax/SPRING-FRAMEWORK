a047e033ee5b25ed12cf011e230476474b5663d3
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 14:15:17 +0100

    Ft | Jwt impl

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/CustomUserDetails.java
@@ -0,0 +1,34 @@
+package dev.InnocentUdo.service.impl;
+
+import dev.InnocentUdo.enums.USER_ROLE;
+import dev.InnocentUdo.model.User;
+import dev.InnocentUdo.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CustomUserDetails implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(username);
+        if(user != null){
+            throw new UsernameNotFoundException("User not found with email " + username);
+        }
+        USER_ROLE role = user.getRole();
+        if(role == null) role = USER_ROLE.ROLE_CUSTOMER;
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(role.toString()));
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), authorities);
+    }
+}
