79639bebf8815a74ab6ea29dff0cb2dc981ead18
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 18:51:00 +0100

    Ft | Jwt impl, login and sign

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/Config/AppConfig.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/Config/AppConfig.java
@@ -33,7 +33,7 @@ public class AppConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()));
 
-        return null;
+        return http.build();
     }
 
     private CorsConfigurationSource corsConfigurationSource() {
--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/controller/AuthController.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/controller/AuthController.java
@@ -1,17 +1,30 @@
 package dev.InnocentUdo.controller;
 
 import dev.InnocentUdo.Config.JwtProvider;
+import dev.InnocentUdo.DTO.request.LoginRequest;
 import dev.InnocentUdo.DTO.response.AuthResponse;
+import dev.InnocentUdo.enums.USER_ROLE;
+import dev.InnocentUdo.model.Cart;
 import dev.InnocentUdo.model.User;
 import dev.InnocentUdo.repository.CartRepository;
 import dev.InnocentUdo.repository.UserRepository;
 import dev.InnocentUdo.service.impl.CustomUserDetails;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/auth")
@@ -31,6 +44,7 @@ public class AuthController {
         this.cartRepository = cartRepository;
     }
 
+    @PostMapping("/signup")
     public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
         User isEmailExist  = userRepository.findByEmail(user.getEmail());
         if(isEmailExist != null){
@@ -44,6 +58,50 @@ public class AuthController {
 
         User savedUser = userRepository.save(createUser);
 
-        return null;
+        Cart cart = new Cart();
+        cart.setCustomer(savedUser);
+        cartRepository.save(cart);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToken(authentication);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register success");
+        authResponse.setRole(savedUser.getRole());
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/signin")
+    public ResponseEntity<AuthResponse> signIn(@RequestBody LoginRequest req){
+        String username = req.getEmail();
+        String password = req.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role = authorities.isEmpty() ? null : authorities.iterator().next().getAuthority();
+
+        String jwt = jwtProvider.generateToken(authentication);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Login success");
+        authResponse.setRole(USER_ROLE.valueOf(role));
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customUserDetails.loadUserByUsername(username);
+
+        if(userDetails == null){
+            throw new BadCredentialsException("Invalid username...");
+        }
+        if(!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid password....");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null,userDetails.getAuthorities());
     }
 }
