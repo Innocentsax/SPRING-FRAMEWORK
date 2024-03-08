01c8463203f9bddc90367ec8d2c64606506c855c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 11:48:11 +0100

    Ft | Entity Creation and design

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/ContactInformation.java
@@ -0,0 +1,11 @@
+package dev.InnocentUdo.model;
+
+import lombok.Data;
+
+@Data
+public class ContactInformation {
+    private String email;
+    private String mobile;
+    private String twitter;
+    private String instagram;
+}
