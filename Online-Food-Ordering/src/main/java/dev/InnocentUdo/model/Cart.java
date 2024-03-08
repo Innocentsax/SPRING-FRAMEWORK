f240c72da3bb1f571690487797f86ac1ba3f4cb6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 11:48:08 +0100

    Ft | Entity Creation and design

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/Cart.java
@@ -0,0 +1,27 @@
+package dev.InnocentUdo.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @OneToOne
+    private User customer;
+
+    private Long total;
+
+    @OneToMany(mappedBy = "cart",cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CartItem> item = new ArrayList<>();
+}
