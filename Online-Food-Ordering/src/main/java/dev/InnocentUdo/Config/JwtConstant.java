1ac870eeddce2fcc9a7540c847202cd908dd2ce9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 14:15:15 +0100

    Ft | Jwt impl

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/Config/JwtConstant.java
@@ -0,0 +1,6 @@
+package dev.InnocentUdo.Config;
+
+public class JwtConstant {
+    public static final String SECRET_KEY = "kjdbvkjhbdkhjabdhvahbiudhiuvhiurhghwqbjdbadvbouasbdfbvajdbjvbouabdvoujabdfvjabdsvjaobdcvaola";
+    public static final String JWT_HEADER = "Authorization";
+}
