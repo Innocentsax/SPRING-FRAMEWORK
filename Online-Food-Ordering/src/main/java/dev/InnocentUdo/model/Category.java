628b32265e25a9779cbc40d63551fbba011e8c4c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-08 11:48:20 +0100

    Ft | Entity Creation and design

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/Category.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/model/Category.java
@@ -1,9 +1,7 @@
 package dev.InnocentUdo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,4 +14,9 @@ public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+    private String name;
+
+    @ManyToOne
+    @JsonIgnore
+    private Restaurant restaurant;
 }
