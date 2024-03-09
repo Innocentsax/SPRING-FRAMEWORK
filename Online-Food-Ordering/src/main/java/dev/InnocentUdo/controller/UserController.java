e8b87ca8fbdeac946908185db108f9ac36dc6464
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-09 20:08:44 +0100

    Ft | Find user with Jwt

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/controller/UserController.java
@@ -0,0 +1,26 @@
+package dev.InnocentUdo.controller;
+
+import dev.InnocentUdo.model.User;
+import dev.InnocentUdo.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/profile")
+    public ResponseEntity<User> findUserByJwtToken(@RequestHeader("Authorization") String jwt) throws Exception{
+        User user = userService.findUserByJwtToken(jwt);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+}
