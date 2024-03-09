38b6f831029885c8c6c1ab177f26feb11fe4feb0
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-09 20:08:48 +0100

    Ft | Find user with Jwt

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/UserServiceImpl.java
@@ -0,0 +1,37 @@
+package dev.InnocentUdo.service.impl;
+
+import dev.InnocentUdo.Config.JwtProvider;
+import dev.InnocentUdo.model.User;
+import dev.InnocentUdo.repository.UserRepository;
+import dev.InnocentUdo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+    private JwtProvider jwtProvider;
+
+    @Autowired
+    public UserServiceImpl(UserRepository userRepository, JwtProvider jwtProvider) {
+        this.userRepository = userRepository;
+        this.jwtProvider = jwtProvider;
+    }
+
+    @Override
+    public User findUserByJwtToken(String jwt) throws Exception {
+        String email = jwtProvider.getEmailFromJwtToken(jwt);
+        User user = findUserByEmail(email);
+        return user;
+    }
+
+    @Override
+    public User findUserByEmail(String email) throws Exception {
+        User user = userRepository.findByEmail(email);
+        if(user == null){
+            throw new Exception("User not found");
+        }
+        return user;
+    }
+}
