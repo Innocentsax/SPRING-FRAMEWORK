0ed589b89a9e0d519e824a9b3c02f6d2fa7c1143
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-09 20:08:42 +0100

    Ft | Find user with Jwt

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/UserService.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/UserService.java
@@ -1,4 +1,8 @@
 package dev.InnocentUdo.service;
 
+import dev.InnocentUdo.model.User;
+
 public interface UserService {
+    public User findUserByJwtToken(String jwt) throws Exception;
+    public User findUserByEmail(String email) throws Exception;
 }
