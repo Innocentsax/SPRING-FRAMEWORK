eb310a83fb973ab4e299812e7e1e8875ef3034c7
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-10 13:08:07 +0100

    Ft | Restaurant and Admin panel

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/DTO/request/CreateRestaurantRequest.java
@@ -0,0 +1,19 @@
+package dev.InnocentUdo.DTO.request;
+
+import dev.InnocentUdo.model.Address;
+import dev.InnocentUdo.model.ContactInformation;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CreateRestaurantRequest {
+    private Long id;
+    private String name;
+    private String description;
+    private String cuisineType;
+    private Address address;
+    private ContactInformation contactInformation;
+    private String openingHours;
+    private List<String> images;
+}
