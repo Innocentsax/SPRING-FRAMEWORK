77c65eeb887eae95e8de64083524909438628937
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-11 19:39:21 +0100

    Ft | Category Service api

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package dev.InnocentUdo.repository;
+
+import dev.InnocentUdo.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    public List<Category> findByRestaurantId(Long id);
+}
