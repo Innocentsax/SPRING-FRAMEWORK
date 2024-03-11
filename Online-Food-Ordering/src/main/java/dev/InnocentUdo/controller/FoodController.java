783b01ddc5fbd2f7d0428fc53b7b12b43aebcd9e
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-11 18:45:30 +0100

    Ft | Food api and Admin panel

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/controller/FoodController.java
@@ -0,0 +1,46 @@
+package dev.InnocentUdo.controller;
+
+import dev.InnocentUdo.model.Food;
+import dev.InnocentUdo.model.User;
+import dev.InnocentUdo.service.FoodService;
+import dev.InnocentUdo.service.RestaurantService;
+import dev.InnocentUdo.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/food")
+public class FoodController {
+    private FoodService foodService;
+    private UserService userService;
+    private RestaurantService restaurantService;
+
+    public FoodController(FoodService foodService, UserService userService, RestaurantService restaurantService) {
+        this.foodService = foodService;
+        this.userService = userService;
+        this.restaurantService = restaurantService;
+    }
+    @GetMapping("/search")
+    public ResponseEntity<List<Food>> searchFood(@RequestParam String name,
+                                                @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        List<Food> foods = foodService.searchFood(name);
+
+        return new ResponseEntity<>(foods, HttpStatus.OK);
+    }
+
+    @GetMapping("/restaurant/{restaurantId}")
+    public ResponseEntity<List<Food>> getRestaurantFood(
+            @RequestParam boolean vegetarian, @RequestParam boolean seasonal,
+            @RequestParam boolean nonVegetarian, @RequestParam(required = false) String food_category,
+            @PathVariable Long restaurantId,
+            @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        List<Food> foods = foodService.getRestaurantsFood(restaurantId, vegetarian, nonVegetarian, seasonal, food_category);
+
+        return new ResponseEntity<>(foods, HttpStatus.OK);
+    }
+}
