6b79bafea37bd12766546864c6c52771755231e8
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-13 16:03:23 +0100

    Ft | Cart Api

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/controller/CartController.java
@@ -0,0 +1,51 @@
+package dev.InnocentUdo.controller;
+
+import dev.InnocentUdo.DTO.request.AddCartItemRequest;
+import dev.InnocentUdo.DTO.request.UpdateCartItemRequest;
+import dev.InnocentUdo.model.Cart;
+import dev.InnocentUdo.model.CartItem;
+import dev.InnocentUdo.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class CartController {
+    @Autowired
+    private CartService cartService;
+
+    @PutMapping("/cart/add")
+    public ResponseEntity<CartItem> addItemToCart(@RequestBody AddCartItemRequest req,
+                                                  @RequestHeader("Authorization") String jwt) throws Exception {
+        CartItem cartItem = cartService.addItemToCart(req, jwt);
+        return new ResponseEntity<>(cartItem, HttpStatus.OK);
+    }
+
+    @PutMapping("/cart-item/update")
+    public ResponseEntity<CartItem> updateCartItemQuantity(@RequestBody UpdateCartItemRequest req,
+                                                  @RequestHeader("Authorization") String jwt) throws Exception {
+        CartItem cartItem = cartService.updateCartItemQuantity(req.getCartItemId(), req.getQuantity());
+        return new ResponseEntity<>(cartItem, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/cart-item/{id}/remove")
+    public ResponseEntity<Cart> removeCartItem(@PathVariable Long id,
+                                               @RequestHeader("Authorization") String jwt) throws Exception {
+        Cart cart = cartService.removeItemFromCart(id, jwt);
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+    }
+
+    @PutMapping("/cart/clear")
+    public ResponseEntity<Cart> clearCart(@RequestHeader("Authorization") String jwt) throws Exception {
+        Cart cart = cartService.clearCart(jwt);
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+    }
+
+    @GetMapping("/cart")
+    public ResponseEntity<Cart> findUserCart(@RequestHeader("Authorization") String jwt) throws Exception {
+        Cart cart = cartService.findCartByUserId(jwt);
+        return new ResponseEntity<>(cart, HttpStatus.OK);
+    }
+}
