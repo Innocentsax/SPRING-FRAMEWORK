5caabb4a4560c4fc51e15266d7034cb3c8c5b51f
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-13 16:03:29 +0100

    Ft | Cart Api

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/CartService.java
@@ -0,0 +1,15 @@
+package dev.InnocentUdo.service;
+
+import dev.InnocentUdo.DTO.request.AddCartItemRequest;
+import dev.InnocentUdo.model.Cart;
+import dev.InnocentUdo.model.CartItem;
+
+public interface CartService {
+    public CartItem addItemToCart(AddCartItemRequest req, String jwt) throws Exception;
+    public CartItem updateCartItemQuantity(Long cartItemId, int quantity) throws Exception;
+    public Cart removeItemFromCart(Long cartItemId, String jwt) throws Exception;
+    public Long calculateCartTotals(Cart cart) throws Exception;
+    public Cart findCartById(Long id) throws Exception;
+    public Cart findCartByUserId(String jwt) throws Exception;
+    public Cart clearCart(String jwt) throws Exception;
+}
