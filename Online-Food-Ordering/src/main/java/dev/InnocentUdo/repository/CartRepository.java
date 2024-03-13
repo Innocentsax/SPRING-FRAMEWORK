e5f77fdf92559b572060a76a0568ae66266bb714
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-13 16:03:27 +0100

    Ft | Cart Api

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/repository/CartRepository.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/repository/CartRepository.java
@@ -4,4 +4,5 @@ import dev.InnocentUdo.model.Cart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartRepository extends JpaRepository<Cart, Long> {
+    public Cart findByCustomerId(Long userId);
 }
