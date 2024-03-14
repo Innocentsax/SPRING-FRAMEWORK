4b98c7061d4b47130f52345b2b049ba9974b2a69
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-14 15:12:32 +0100

    Ft | Order Api

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/CartServiceImpl.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/CartServiceImpl.java
@@ -102,15 +102,15 @@ public class CartServiceImpl implements CartService {
     }
 
     @Override
-    public Cart findCartByUserId(String jwt) throws Exception {
-        User user = userService.findUserByJwtToken(jwt);
-        return cartRepository.findByCustomerId(user.getId());
+    public Cart findCartByUserId(Long userId) throws Exception {
+        Cart cart = cartRepository.findByCustomerId(userId);
+        cart.setTotal(calculateCartTotals(cart));
+        return cart;
     }
 
     @Override
-    public Cart clearCart(String jwt) throws Exception {
-        User user = userService.findUserByJwtToken(jwt);
-        Cart cart = findCartByUserId(jwt);
+    public Cart clearCart(Long userId) throws Exception {
+        Cart cart = findCartByUserId(userId);
         cart.getItem().clear();
         return cartRepository.save(cart);
     }
