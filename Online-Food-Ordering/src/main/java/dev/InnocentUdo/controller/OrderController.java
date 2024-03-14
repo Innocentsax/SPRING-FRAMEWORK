cd2b919996f29692f37b7060533b2acb38a1d594
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-14 15:12:43 +0100

    Ft | Order Api

---
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/controller/OrderController.java
@@ -0,0 +1,40 @@
+package dev.InnocentUdo.controller;
+
+import dev.InnocentUdo.DTO.request.OrderRequest;
+import dev.InnocentUdo.model.Order;
+import dev.InnocentUdo.model.User;
+import dev.InnocentUdo.service.OrderService;
+import dev.InnocentUdo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api")
+public class OrderController {
+    private OrderService orderService;
+    private UserService userService;
+    @Autowired
+    public OrderController(OrderService orderService, UserService userService) {
+        this.orderService = orderService;
+        this.userService = userService;
+    }
+    @PostMapping("/order")
+    public ResponseEntity<Order> createOrder(@RequestBody OrderRequest req,
+                                               @RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        Order order = orderService.createOrder(req, user);
+        return new ResponseEntity<>(order, HttpStatus.OK);
+    }
+
+    @GetMapping("/order/user")
+    public ResponseEntity<List<Order>> getOrderHistory(@RequestHeader("Authorization") String jwt) throws Exception {
+        User user = userService.findUserByJwtToken(jwt);
+        List<Order> orders = orderService.getUsersOrder(user.getId());
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+}
