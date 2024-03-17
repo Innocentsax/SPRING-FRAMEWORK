8a09fcc53a5e5366e09a75bec49fbb4e37dc494c
Innocent Udo <innocentcharlesudo@gmail.com>
2024-03-17 15:52:56 +0100

    Online Food Ordering Website

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/PaymentService.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/PaymentService.java
@@ -1,8 +1,9 @@
 package dev.InnocentUdo.service;
 
+import com.stripe.exception.StripeException;
 import dev.InnocentUdo.DTO.response.PaymentResponse;
 import dev.InnocentUdo.model.Order;
 
 public interface PaymentService {
-    public PaymentResponse createPaymentLink(Order order);
+    public PaymentResponse createPaymentLink(Order order) throws StripeException;
 }
