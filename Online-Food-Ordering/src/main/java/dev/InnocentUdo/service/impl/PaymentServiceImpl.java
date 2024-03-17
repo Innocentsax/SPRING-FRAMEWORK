c7b263208f61c778989dbdaf77ba44e63787eaf1
Innocent Udo <innocentcharlesudo@gmail.com>
2024-03-17 15:52:51 +0100

    Online Food Ordering Website

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/PaymentServiceImpl.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/PaymentServiceImpl.java
@@ -1,5 +1,9 @@
 package dev.InnocentUdo.service.impl;
 
+import com.stripe.Stripe;
+import com.stripe.exception.StripeException;
+import com.stripe.model.checkout.Session;
+import com.stripe.param.checkout.SessionCreateParams;
 import dev.InnocentUdo.DTO.response.PaymentResponse;
 import dev.InnocentUdo.model.Order;
 import dev.InnocentUdo.service.PaymentService;
@@ -11,7 +15,29 @@ public class PaymentServiceImpl implements PaymentService {
     @Value("${stripe.api.key}")
     private String stripeSecretKey;
     @Override
-    public PaymentResponse createPaymentLink(Order order) {
-        return null;
+    public PaymentResponse createPaymentLink(Order order) throws StripeException {
+        Stripe.apiKey = stripeSecretKey;
+        SessionCreateParams params = SessionCreateParams.builder()
+                .addPaymentMethodType(SessionCreateParams.PaymentMethodType.CARD)
+                .setMode(SessionCreateParams.Mode.PAYMENT)
+                .setSuccessUrl("http/localhost:3000/payment/success" + order.getId())
+                .setCancelUrl("http://localhost:3000/payment/fail")
+                .addLineItem(SessionCreateParams.LineItem.builder()
+                        .setQuantity(1L).setPriceData(SessionCreateParams.LineItem.PriceData.builder()
+                                .setCurrency("usd")
+                                .setUnitAmount((long) order.getTotalAmount() * 100)
+                                .setProductData(SessionCreateParams.LineItem.PriceData.ProductData.builder()
+                                        .setName("Online-Food-Store")
+                                        .build())
+                                .build())
+                        .build())
+                .build();
+
+        Session session = Session.create(params);
+
+        PaymentResponse res = new PaymentResponse();
+        res.setPayment_url(session.getUrl());
+
+        return res;
     }
 }
