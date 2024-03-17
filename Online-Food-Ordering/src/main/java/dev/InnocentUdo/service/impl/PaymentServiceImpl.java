e8be75130b521e2a3024e52e354501d66e6249de
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-17 14:35:51 +0100

    Ft | Payment API Api

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/PaymentServiceImpl.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/PaymentServiceImpl.java
@@ -3,11 +3,13 @@ package dev.InnocentUdo.service.impl;
 import dev.InnocentUdo.DTO.response.PaymentResponse;
 import dev.InnocentUdo.model.Order;
 import dev.InnocentUdo.service.PaymentService;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
-
+    @Value("${stripe.api.key}")
+    private String stripeSecretKey;
     @Override
     public PaymentResponse createPaymentLink(Order order) {
         return null;
