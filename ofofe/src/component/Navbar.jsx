9b7cc927c7186ef6a8e246a3a92d314914a8ba38
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-20 21:20:25 +0100

    Navbar

--- ofofe/src/component/Navbar.jsx
+++ ofofe/src/component/Navbar.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import { Avatar, Badge, IconButton } from "@mui/material";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import "./Navbar.css";
 
 const Navbar = () => {
   return (
@@ -6,8 +10,25 @@ const Navbar = () => {
       <div className="flex items-center space-x-4">
         <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
           <li className="logo font-semibold text-grey-300 text-2x1">
-            Calabar Kitchen
+            CALABAR KITCHEN
           </li>
+        </div>
+      </div>
+      <div className="flex items-center space-x-2 lg:space-x-10">
+        <div className="">
+          <IconButton>
+            <SearchIcon sx={{ fontSize: "1.5rem" }} />
+          </IconButton>
+        </div>
+        <div className="">
+          <Avatar sx={{ bgcolor: "white" }}>C</Avatar>
+        </div>
+        <div className="">
+          <IconButton>
+            <Badge color="blue" badgeContent={3}>
+              <ShoppingCartIcon sx={{ fontSize: "1.5rem" }} />
+            </Badge>
+          </IconButton>
         </div>
       </div>
     </div>
