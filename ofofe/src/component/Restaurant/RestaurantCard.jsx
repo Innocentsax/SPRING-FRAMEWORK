1bf70aa4ca678aeb8aadb009c9a953710010d2c6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-21 16:01:36 +0100

    Restaurant

---
+++ ofofe/src/component/Restaurant/RestaurantCard.jsx
@@ -0,0 +1,45 @@
+import { Card, Chip, IconButton } from "@mui/material";
+import React from "react";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+
+const RestaurantCard = () => {
+  return (
+    <Card className="w-[18rem]">
+      <div
+        className={`${true ? "cursor-pointer" : "cursor-not-allowed"} relative`}
+      >
+        <img
+          className="w-full h-[10rem] rounded-t-md object-cover"
+          src="https://images.pexels.com/photos/1579739/pexels-photo-1579739.jpeg?auto=compress&cs=tinysrgb&w=800"
+          alt=""
+        />
+        <Chip
+          size="small"
+          className="absolute top-2 left-2"
+          color={true ? "success" : "error"}
+          label={true ? "open" : "closed"}
+        />
+      </div>
+      <div className="p-4 textPart lg:flex w-full justify-between">
+        <div className="space-y-1">
+          <p className="font-semibold text-lg">TransCorp Hotel</p>
+          <p className="text-grey-500 text-sm">
+            Craving it all? Dive into our global 5 Star...
+          </p>
+        </div>
+        <div>
+          <IconButton>
+            {true ? (
+              <FavoriteIcon />
+            ) : (
+              <FavoriteBorderIcon></FavoriteBorderIcon>
+            )}
+          </IconButton>
+        </div>
+      </div>
+    </Card>
+  );
+};
+
+export default RestaurantCard;
