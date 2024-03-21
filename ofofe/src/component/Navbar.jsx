223623502245e87f97d90b2e76736dcebc4bb7f5
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-21 13:15:11 +0100

    Navbar

--- ofofe/src/component/Navbar.jsx
+++ ofofe/src/component/Navbar.jsx
@@ -6,7 +6,7 @@ import "./Navbar.css";
 
 const Navbar = () => {
   return (
-    <div className="px-5 x-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between">
+    <div className="px-5 x-50 py-[.8rem] bg-[#a21ee9] lg:px-20 flex justify-between">
       <div className="flex items-center space-x-4">
         <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
           <li className="logo font-semibold text-grey-300 text-2xl">
