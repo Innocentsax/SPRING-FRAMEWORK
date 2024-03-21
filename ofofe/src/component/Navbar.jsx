acb421b6aa719ef7182180b3b8c765ace4d679bc
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-21 10:10:59 +0100

    Navbar

--- ofofe/src/component/Navbar.jsx
+++ ofofe/src/component/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
     <div className="px-5 x-50 py-[.8rem] bg-[#e91e63] lg:px-20 flex justify-between">
       <div className="flex items-center space-x-4">
         <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
-          <li className="logo font-semibold text-grey-300 text-2x1">
+          <li className="logo font-semibold text-grey-300 text-2xl">
             CALABAR KITCHEN
           </li>
         </div>
