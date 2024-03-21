bc6ed5314bde81dc5c39f93f19a9c3978ff44ef4
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-21 16:02:04 +0100

    Update App

--- ofofe/src/App.js
+++ ofofe/src/App.js
@@ -4,13 +4,15 @@ import Navbar from "./component/Navbar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { darkTheme } from "./Theme/DarkTheme";
 import Home from "./component/Home/Home";
+import RestaurantDetails from "./component/Restaurant/RestaurantDetails";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      <RestaurantDetails />
     </ThemeProvider>
   );
 }
