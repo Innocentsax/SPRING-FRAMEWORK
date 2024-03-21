d8cadc74f975c7a2f2fab94d31ae93035183b447
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-21 13:14:56 +0100

    Top meels

---
+++ ofofe/src/component/Home/topMeel.js
@@ -0,0 +1,62 @@
+export const topMeels = [
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2024/03/01/08/44/pizza-8605916_1280.jpg",
+    title: "Pizza bazil pepperoni",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2024/03/15/16/25/pizza-8635314_1280.jpg",
+    title: "Pizza food round",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2017/12/09/08/18/pizza-3007395_1280.jpg",
+    title: "Pizza Italian",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2017/07/28/14/28/macarons-2548818_1280.jpg",
+    title: "Macarons",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2016/06/12/15/03/cupcakes-1452178_1280.jpg",
+    title: "cupcakes",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2023/08/26/09/04/strawberry-8214486_1280.jpg",
+    title: "strawberry cake",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_1280.jpg",
+    title: "Pancakes",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2014/12/22/16/50/pancake-577386_1280.jpg",
+    title: "Crepes",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/29/04/49/blueberries-1867398_1280.jpg",
+    title: "blueberries",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2020/11/04/13/29/cereal-5712343_1280.jpg",
+    title: "Cereal Foods",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/18/19/00/bread-1836411_1280.jpg",
+    title: "bread eggs",
+  },
+  {
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/06/23/16/breakfast-1804436_1280.jpg",
+    title: "Breakfast",
+  },
+];
