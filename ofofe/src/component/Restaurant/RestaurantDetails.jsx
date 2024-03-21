8fa0e736d9e67ce96956eb21a45c734dfb76c8c1
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-21 16:01:51 +0100

    Restaurant

---
+++ ofofe/src/component/Restaurant/RestaurantDetails.jsx
@@ -0,0 +1,17 @@
+import { Grid } from "@mui/material";
+import React from "react";
+
+const RestaurantDetails = () => {
+  return (
+    <div className="px-5 lg:px-20">
+      <section>
+        <h3>Home/Efik/TransCorp Hotel</h3>
+        <div>
+          <Grid></Grid>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default RestaurantDetails;
