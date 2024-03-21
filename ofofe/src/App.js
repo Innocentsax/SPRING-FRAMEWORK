1034e8edcd2d7466dc35ae6ed0dd303a24c79e4b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-21 10:11:09 +0100

    Route

--- ofofe/src/App.js
+++ ofofe/src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import Navbar from "./component/Navbar";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { darkTheme } from "./Theme/DarkTheme";
+import Home from "./component/Home/Home";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
       <Navbar />
+      <Home />
     </ThemeProvider>
   );
 }
