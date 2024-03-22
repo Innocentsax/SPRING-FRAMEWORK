6f501cb0f21db986b0cbfc26bd3b5fc5e3c9457c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-22 11:07:59 +0100

    Restaurant Details

--- ofofe/src/component/Restaurant/RestaurantDetails.jsx
+++ ofofe/src/component/Restaurant/RestaurantDetails.jsx
@@ -1,13 +1,54 @@
 import { Grid } from "@mui/material";
 import React from "react";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CalendarTodayIcon from "@mui/icons-material/CalendarToday";
 
 const RestaurantDetails = () => {
   return (
     <div className="px-5 lg:px-20">
       <section>
-        <h3>Home/Efik/TransCorp Hotel</h3>
+        <h3 className="text-gray-500 py-2 mt-10">Home/Efik/TransCorp Hotel</h3>
         <div>
-          <Grid></Grid>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <img
+                className="w-full h-[40vh] object-cover"
+                src="https://images.pexels.com/photos/460537/pexels-photo-460537.jpeg?auto=compress&cs=tinysrgb&w=800"
+              />
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <img
+                className="w-full h-[40vh] object-cover"
+                src="https://images.pexels.com/photos/1307698/pexels-photo-1307698.jpeg?auto=compress&cs=tinysrgb&w=800"
+              />
+            </Grid>
+            <Grid item xs={12} lg={6}>
+              <img
+                className="w-full h-[40vh] object-cover"
+                src="https://images.pexels.com/photos/2696064/pexels-photo-2696064.jpeg?auto=compress&cs=tinysrgb&w=800"
+              />
+            </Grid>
+          </Grid>
+        </div>
+        <div className="pt-3 pb-5">
+          <h1 className="text-4xl font-semibold">TransCorp Hotel</h1>
+          <p className="text-gray-500 mt-1">
+            The Transcorp Hotel Calabar is a luxurious hotel located in Oguwo
+            Street, Box Hill, Calabar in Nigeria's Cross River State. It offers
+            comfortable and elegant accommodations, fine dining options, and
+            recreational facilities such as a fitness center, swimming pool, and
+            a rooftop bar.
+          </p>
+          <div className="space-y-3 mt-3">
+            <p className="text-gray-500 flex items-center gap-3">
+              <LocationOnIcon />
+              <span>Stadium Road, Calabar.</span>
+            </p>
+            <p className="text-gray-500 flex items-center gap-3">
+              <CalendarTodayIcon />
+              <span>Mon-Sun: 9:00 AM - 9:00 PM (Today)</span>
+            </p>
+          </div>
         </div>
       </section>
     </div>
