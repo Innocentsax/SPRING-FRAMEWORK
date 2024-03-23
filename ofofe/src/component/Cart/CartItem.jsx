4442e2e2c6d623b3a965f7b924624e4fbcbdbccb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-23 19:14:05 +0100

    CartItems

---
+++ ofofe/src/component/Cart/CartItem.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
+import { IconButton } from "@mui/material";
+
+const CartItem = () => {
+  return (
+    <div className="px5">
+      <div className="lg:flex items-center lg:space-x-5">
+        <div>
+          <img
+            className="w-[5rem] h-[5rem] object-cover"
+            src="https://cdn.pixabay.com/photo/2018/01/22/16/28/pancake-3099315_1280.jpg"
+            alt=""
+          />
+        </div>
+        <div className="flex items-center justify-between lg:w-[70%]">
+          <div className="space-y-1 lg:space-y-3 w-full">
+            <p>Pancake</p>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center space-x-1">
+                <IconButton>
+                  <RemoveCircleOutlineIcon />
+                </IconButton>
+                <div className="w-5 h-5 text-xs flex items-center justify-center">
+                  {5}
+                </div>
+                <IconButton>
+                  <AddCircleOutlineIcon />
+                </IconButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
