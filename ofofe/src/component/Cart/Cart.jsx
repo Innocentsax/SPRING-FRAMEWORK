dc40127a5ac3f5f192e8ab4e4e9507520129c6be
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-24 10:23:11 +0100

    Cart

--- ofofe/src/component/Cart/Cart.jsx
+++ ofofe/src/component/Cart/Cart.jsx
@@ -2,14 +2,21 @@ import { Divider } from "@mui/material";
 import React from "react";
 import CartItem from "./CartItem";
 
+const items = [1, 1];
 const Cart = () => {
   return (
     <div>
       <main className="lg:flex justify-between">
         <section className="lg:w-[30%] space-y-6 lg:min-h-screen pt-10">
-          <CartItem />
+          {items.map((item) => (
+            <CartItem />
+          ))}
+          <Divider />
+          <div className="billDetails px-5 text-sm">
+            <p className="font-extralight py-5">Bill Details</p>
+            <div className="space-y-3"></div>
+          </div>
         </section>
-        <Divider />
       </main>
     </div>
   );
