2af8fcc08dbc46ce5d4bcc97baf6a394bed39871
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-26 19:16:29 +0100

    Card

--- ofofe/src/component/Cart/Cart.jsx
+++ ofofe/src/component/Cart/Cart.jsx
@@ -1,11 +1,56 @@
-import { Divider } from "@mui/material";
+import {
+  Box,
+  Button,
+  Card,
+  Divider,
+  Grid,
+  Modal,
+  TextField,
+} from "@mui/material";
 import React from "react";
 import CartItem from "./CartItem";
+import AddressCard from "./AddressCard";
+import AddLocationAltIcon from "@mui/icons-material/AddLocationAlt";
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  outline: "none",
+  boxShadow: 24,
+  p: 4,
+};
+
+const initialValue = {
+  streetAddress: "",
+  state: "",
+  pincode: "",
+  city: "",
+};
+const validationSchema = Yup.object().shape({
+  streetAddress: Yup.string().required("Street address is Required"),
+  state: Yup.string().required("State isRequired"),
+  pincode: Yup.string().required("Pincode is Required"),
+  city: Yup.string().required("City is Required"),
+});
 
 const items = [1, 1];
 const Cart = () => {
+  const createOrderUsingSelectedAddress = () => {};
+  const handleOpenAddressModel = () => setOpen(true);
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleSubmit = (value) => {
+    console.log("form value ", value);
+  };
   return (
-    <div>
+    <>
       <main className="lg:flex justify-between">
         <section className="lg:w-[30%] space-y-6 lg:min-h-screen pt-10">
           {items.map((item) => (
@@ -14,11 +59,139 @@ const Cart = () => {
           <Divider />
           <div className="billDetails px-5 text-sm">
             <p className="font-extralight py-5">Bill Details</p>
-            <div className="space-y-3"></div>
+            <div className="space-y-3">
+              <div className="flex justify-between text text-grey-400">
+                <p>Item Total</p>
+                <p>₦3000</p>
+              </div>
+              <div className="flex justify-between text text-grey-400">
+                <p>Delivery fee</p>
+                <p>₦200</p>
+              </div>
+              <div className="flex justify-between text text-grey-400">
+                <p>GST and Restaurant Charges</p>
+                <p>₦200</p>
+              </div>
+              <Divider />
+            </div>
+            <div className="flex justify-between text-gray-400">
+              <p>Total pay</p>
+              <p>₦3400</p>
+            </div>
+          </div>
+        </section>
+        <Divider orientation="vertical" flexItem />
+        <section className="lg:w-[70%] flex justify-center px-5 pb-10 lg:pb-0">
+          <div>
+            <h1 className="text-center font-semibold text-2xl py-10">
+              Choose Delivery Address
+            </h1>
+            <div className="flex gap-5 flex-wrap justify-center">
+              {[1, 1, 1, 1, 1].map((item) => (
+                <AddressCard
+                  handleSelectAddress={createOrderUsingSelectedAddress}
+                  item={item}
+                  showButton={true}
+                />
+              ))}
+              <Card className="flex gap-5 w-64 p-5">
+                <AddLocationAltIcon />
+                <div className="space-y-3 text-gray-500">
+                  <h1 className="font-semibold text-lg text-white">
+                    Add new Address
+                  </h1>
+                  <Button
+                    variant="outlined"
+                    fullWidth
+                    onClick={handleOpenAddressModel}
+                  >
+                    Add
+                  </Button>
+                </div>
+              </Card>
+            </div>
           </div>
         </section>
       </main>
-    </div>
+      <Button onClick={handleOpen}>Open modal</Button>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Formik
+            initialValues={initialValue}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+          >
+            {({ errors, touched }) => (
+              <Form>
+                <Grid container spacing={2}>
+                  <Grid item xs={12}>
+                    <Field
+                      as={TextField}
+                      name="streetAddress"
+                      label="Street Address"
+                      fullWidth
+                      variant="outlined"
+                      error={
+                        touched.streetAddress && Boolean(errors.streetAddress)
+                      }
+                      helperText={touched.streetAddress && errors.streetAddress}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field
+                      as={TextField}
+                      name="state"
+                      label="State"
+                      fullWidth
+                      variant="outlined"
+                      error={touched.state && Boolean(errors.state)}
+                      helperText={touched.state && errors.state}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field
+                      as={TextField}
+                      name="city"
+                      label="city"
+                      fullWidth
+                      variant="outlined"
+                      error={touched.city && Boolean(errors.city)}
+                      helperText={touched.city && errors.city}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Field
+                      as={TextField}
+                      name="pincode"
+                      label="Pincode"
+                      fullWidth
+                      variant="outlined"
+                      error={touched.pincode && Boolean(errors.pincode)}
+                      helperText={touched.pincode && errors.pincode}
+                    />
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Button
+                      fullWidth
+                      variant="contained"
+                      type="submit"
+                      color="primary"
+                    >
+                      Deliver Here
+                    </Button>
+                  </Grid>
+                </Grid>
+              </Form>
+            )}
+          </Formik>
+        </Box>
+      </Modal>
+    </>
   );
 };
 
