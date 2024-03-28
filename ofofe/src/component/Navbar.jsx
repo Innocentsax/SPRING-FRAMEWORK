e0015e2f0ce935d7601fd1b556d53450b25bd4e0
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-28 11:00:15 +0100

    Update Navbar

--- ofofe/src/component/Navbar.jsx
+++ ofofe/src/component/Navbar.jsx
@@ -3,10 +3,15 @@ import SearchIcon from "@mui/icons-material/Search";
 import { Avatar, Badge, IconButton } from "@mui/material";
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import "./Navbar.css";
+import { Box } from "@mui/system";
+import zIndex from "@mui/material/styles/zIndex";
 
 const Navbar = () => {
   return (
-    <div className="px-5 x-50 py-[.8rem] bg-[#a21ee9] lg:px-20 flex justify-between">
+    <Box
+      sx={{ zIndex: 100 }}
+      className="px-5 sticky top-0 x-50 py-[.8rem] bg-[#a21ee9] lg:px-20 flex justify-between"
+    >
       <div className="flex items-center space-x-4">
         <div className="lg:mr-10 cursor-pointer flex items-center space-x-4">
           <li className="logo font-semibold text-grey-300 text-2xl">
@@ -31,7 +36,7 @@ const Navbar = () => {
           </IconButton>
         </div>
       </div>
-    </div>
+    </Box>
   );
 };
 
