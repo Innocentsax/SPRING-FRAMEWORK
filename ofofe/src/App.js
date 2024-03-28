11dcb18d144f82af739c7bcb8c96283988e886ee
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-28 11:00:56 +0100

    App

--- ofofe/src/App.js
+++ ofofe/src/App.js
@@ -6,15 +6,19 @@ import { darkTheme } from "./Theme/DarkTheme";
 import Home from "./component/Home/Home";
 import RestaurantDetails from "./component/Restaurant/RestaurantDetails";
 import Cart from "./component/Cart/Cart";
+import Profile from "./component/Profile/Profile";
+import CustomerRoute from "./Routers/CustomerRoute";
 
 function App() {
   return (
     <ThemeProvider theme={darkTheme}>
       <CssBaseline />
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <Home /> */}
       {/* <RestaurantDetails /> */}
-      <Cart />
+      {/* <Cart /> */}
+      {/* <Profile /> */}
+      <CustomerRoute />
     </ThemeProvider>
   );
 }
