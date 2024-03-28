852f26ae78cbf05ae9679d5fa81cebe3770221f9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-28 10:59:58 +0100

    UserProfile

---
+++ ofofe/src/component/Profile/UserProfile.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { Button } from "@mui/material";
+
+const UserProfile = () => {
+  const handleLogout = () => {};
+  return (
+    <div className="min-h-screen flex flex-col justify-center item-center text-center">
+      <div className="flex flex-col items-center justify-center">
+        <AccountCircleIcon sx={{ fontSize: "9rem" }} />
+        <h1 className="py-5 text-2xl font-semibold">Innocent Udo</h1>
+        <p>Email: innocentcharlesudo@gmail.com</p>
+        <Button
+          variant="contained"
+          onClick={handleLogout}
+          sx={{ margin: "2rem 0rem" }}
+        >
+          Logout
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfile;
