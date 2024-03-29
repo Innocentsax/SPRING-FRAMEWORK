eb3e08e691ddef7f498ed409f374bad1302499e3
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-30 00:13:48 +0100

    Profile

--- ofofe/src/component/Profile/Profile.jsx
+++ ofofe/src/component/Profile/Profile.jsx
@@ -4,22 +4,22 @@ import { Route, Routes } from "react-router-dom";
 import UserProfile from "./UserProfile";
 import Orders from "./Orders";
 import Address from "./Address";
-import Favorites from "./Favorites";
 import Event from "./Event";
+import Favorite from "./Favorites";
 
 const Profile = () => {
   const [openSideBar, setOpenSideBar] = useState(false);
   return (
-    <div className="lg:flex justify-between">
+    <div className="lg:flex items-center justify-center">
       <div className="sticky h-[80vh] lg:w-[20%]">
         <ProfileNavigation open={openSideBar} />
       </div>
-      <div className="lg:w-[80%]"></div>
+      {/* <div className="lg:w-[80%]"></div> */}
       <Routes>
         <Route path="/" element={<UserProfile />} />
         <Route path="/orders" element={<Orders />} />
         <Route path="/address" element={<Address />} />
-        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/favorites" element={<Favorite />} />
         <Route path="/events" element={<Event />} />
       </Routes>
     </div>
