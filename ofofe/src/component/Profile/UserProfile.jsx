e09b2a7a9a525d5a4ffaf84cbbc101dba00a794d
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-30 00:13:59 +0100

    UserProfile

--- ofofe/src/component/Profile/UserProfile.jsx
+++ ofofe/src/component/Profile/UserProfile.jsx
@@ -5,7 +5,7 @@ import { Button } from "@mui/material";
 const UserProfile = () => {
   const handleLogout = () => {};
   return (
-    <div className="min-h-screen flex flex-col justify-center item-center text-center">
+    <div className="min-h-[80vh] flex flex-col justify-center align-items-center text-center w-full">
       <div className="flex flex-col items-center justify-center">
         <AccountCircleIcon sx={{ fontSize: "9rem" }} />
         <h1 className="py-5 text-2xl font-semibold">Innocent Udo</h1>
