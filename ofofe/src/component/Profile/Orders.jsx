c329a00d6da067728b4eaf4cc568402dcec10c59
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-03-30 00:13:34 +0100

    Order

--- ofofe/src/component/Profile/Orders.jsx
+++ ofofe/src/component/Profile/Orders.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import OrderCard from "./OrderCard";
 
 const Orders = () => {
-  return <div></div>;
+  return (
+    <div className="flex items-center flex-col">
+      <h1 className="text-xl text-center py-7 font-semibold">My Order</h1>
+      <div className="space-y-5 w-full ">
+        {[1, 1, 1, 1].map((item) => (
+          <OrderCard />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Orders;
