7fcc94316c9c0e9fe36f3856bebe496e64277d5b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-02 08:07:30 +0100

    Fav

--- ofofe/src/component/Profile/Favorites.jsx
+++ ofofe/src/component/Profile/Favorites.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import RestaurantCard from "../Restaurant/RestaurantCard";
 
 const favorites = () => {
-  return <div>fav</div>;
+  return (
+    <div>
+      <h1 className="py-5 text-xl font-semibold text-center">My Favorites</h1>
+      <div className="flex flex-wrap gap-3 justify-center">
+        {[1, 1, 1].map((item) => (
+          <RestaurantCard />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default favorites;
