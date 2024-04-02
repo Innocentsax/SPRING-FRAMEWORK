aa815af92508eec975bd671d2dccf28699a64d50
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-02 08:07:50 +0100

    Order

--- ofofe/src/component/Profile/Orders.jsx
+++ ofofe/src/component/Profile/Orders.jsx
@@ -5,7 +5,7 @@ const Orders = () => {
   return (
     <div className="flex items-center flex-col">
       <h1 className="text-xl text-center py-7 font-semibold">My Order</h1>
-      <div className="space-y-5 w-full ">
+      <div className="space-y-5 w-full lg:w-1/2">
         {[1, 1, 1, 1].map((item) => (
           <OrderCard />
         ))}
