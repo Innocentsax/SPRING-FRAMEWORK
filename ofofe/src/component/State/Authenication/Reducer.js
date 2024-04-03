1ea12873f35547b8af6b380e4fcd6b3ada837397
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-03 07:05:01 +0100

    Reducer

---
+++ ofofe/src/component/State/Authenication/Reducer.js
@@ -0,0 +1,18 @@
+const initialState = {
+  user: null,
+  isLoading: false,
+  error: null,
+  jwt: null,
+  favorites: [],
+  success: null,
+};
+
+const authReducer = (state = initialState, action) => {
+  switch (key) {
+    case value:
+      break;
+
+    default:
+      break;
+  }
+};
