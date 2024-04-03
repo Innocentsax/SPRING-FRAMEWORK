ae7afdd16cab27258289804593ce2afa9f30647b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-03 07:04:10 +0100

    Register

--- ofofe/src/component/Auth/RegisterForm.jsx
+++ ofofe/src/component/Auth/RegisterForm.jsx
@@ -1,7 +1,94 @@
 import React from "react";
+import {
+  Button,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { Field, Form, Formik } from "formik";
+import { useNavigate } from "react-router-dom";
+
+const initialValue = {
+  fullName: "",
+  email: "",
+  password: "",
+  role: "",
+};
 
 const RegisterForm = () => {
-  return <div></div>;
+  const handleSubmit = (values) => {
+    console.log(values);
+  };
+  const navigate = useNavigate();
+  return (
+    <div>
+      <Typography className="text-center" variant="h5">
+        Register
+      </Typography>
+      <Formik onSubmit={handleSubmit} initialValues={initialValue}>
+        <Form>
+          <Field
+            as={TextField}
+            name="fullName"
+            label="Full Name"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          />
+          <Field
+            as={TextField}
+            name="email"
+            label="Email"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          />
+
+          <Field
+            as={TextField}
+            name="password"
+            label="Password"
+            fullWidth
+            variant="outlined"
+            margin="normal"
+          />
+          <FormControl fullWidth margin="normal">
+            <InputLabel id="role-simple-select-label">Role</InputLabel>
+            <Field
+              as={Select}
+              labelId="role-simple-select-label"
+              id="role-simple-select"
+              name="role"
+              label="role"
+            >
+              <MenuItem value={"ROLE_CUSTOMER"}>Customer</MenuItem>
+              <MenuItem value={"ROLE_RESTAURANT_OWNER"}>
+                Restaurant Owner
+              </MenuItem>
+            </Field>
+          </FormControl>
+
+          <Button
+            sx={{ mt: 2, padding: "1rem" }}
+            fullWidth
+            type="submit"
+            variant="contained"
+          >
+            Register
+          </Button>
+        </Form>
+      </Formik>
+      <Typography variant="body2" align="center" sx={{ mt: 3 }}>
+        Have an account already?
+        <Button size="small" onClick={() => navigate("/account/login")}>
+          Login
+        </Button>
+      </Typography>
+    </div>
+  );
 };
 
 export default RegisterForm;
