70460e2e5859774e136552e2b0734c85eadee804
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-13 12:51:10 +0100

    Menu

---
+++ ofofe/src/component/State/Menu/Reducer.js
@@ -0,0 +1,79 @@
+import * as actionTypes from "./ActionType";
+
+const initialState = {
+  menuItems: [],
+  loading: false,
+  error: null,
+  search: [],
+  message: null,
+};
+
+const menuItemsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.CREATE_MENU_ITEM_REQUEST:
+    case actionTypes.GET_MENU_ITEMS_BY_RESTAURANT_ID_REQUEST:
+    case actionTypes.DELETE_MENU_ITEM_REQUEST:
+    case actionTypes.SEARCH_MENU_ITEM_REQUEST:
+    case actionTypes.UPDATE_MENU_ITEMS_AVAILABILITY_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+        message: null,
+      };
+    case actionTypes.CREATE_MENU_ITEM_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        menuItems: [...state.menuItems, action.payload],
+        message: "You have successfully created",
+      };
+    case actionTypes.DELETE_MENU_ITEM_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        menuItems: state.menuItems.filter(
+          (menuItem) => menuItem.id !== actionTypes.payload
+        ),
+        message: "You have successfully deleted",
+      };
+    case actionTypes.GET_MENU_ITEMS_BY_RESTAURANT_ID_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        menuItems: action.payload,
+      };
+    case actionTypes.SEARCH_MENU_ITEM_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        search: action.payload,
+      };
+    case actionTypes.UPDATE_MENU_ITEMS_AVAILABILITY_SUCCESS:
+      console.log("updated items id ", action.payload.id);
+      return {
+        ...state,
+        loading: false,
+        menuItems: state.menuItems.map((menuItem) =>
+          menuItem.id === action.payload.id ? action.payload : menuItem
+        ),
+        message: "You have successfully updated",
+      };
+
+    case actionTypes.CREATE_MENU_ITEM_FAILURE:
+    case actionTypes.GET_MENU_ITEMS_BY_RESTAURANT_ID_FAILURE:
+    case actionTypes.DELETE_MENU_ITEM_FAILURE:
+    case actionTypes.SEARCH_MENU_ITEM_FAILURE:
+    case actionTypes.UPDATE_MENU_ITEMS_AVAILABILITY_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload,
+        message: null,
+      };
+
+    default:
+      return state;
+  }
+};
+export default menuItemsReducer;
