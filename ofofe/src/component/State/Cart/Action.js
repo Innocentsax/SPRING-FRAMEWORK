03385988b5f4c1ac4e8299843d3a6ff7f77eb751
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-14 20:50:30 +0100

    Cart

--- ofofe/src/component/State/Cart/Action.js
+++ ofofe/src/component/State/Cart/Action.js
@@ -3,6 +3,9 @@ import {
   ADD_ITEM_TO_CART_FAILURE,
   ADD_ITEM_TO_CART_REQUEST,
   ADD_ITEM_TO_CART_SUCCESS,
+  CLEAR_CART_FAILURE,
+  CLEAR_CART_REQUEST,
+  CLEAR_CART_SUCCESS,
   FIND_CART_FAILURE,
   FIND_CART_REQUEST,
   FIND_CART_SUCCESS,
@@ -99,6 +102,24 @@ export const removeCartItem = ({ cartItemId, jwt }) => {
     } catch (error) {
       console.log("error", error);
       dispatch({ type: REMOVE_CART_ITEM_FAILURE, payload: error.message });
+    }
+  };
+};
+
+export const clearCartAction = () => {
+  return async (dispatch) => {
+    dispatch({ type: CLEAR_CART_REQUEST });
+    try {
+      const { data } = await api.put(`/api/cart/clear`, {
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+        },
+      });
+      console.log("clear cart ", data);
+      dispatch({ type: CLEAR_CART_SUCCESS, payload: data });
+    } catch (error) {
+      console.log("error", error);
+      dispatch({ type: CLEAR_CART_FAILURE, payload: error.message });
     }
   };
 };
