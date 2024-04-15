f296c8b4910854dab8646a3c06e2b1b29501cedb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-15 20:17:29 +0100

    Order

---
+++ ofofe/src/component/State/Order/Reducer.js
@@ -0,0 +1,38 @@
+import { GET_USER_SUCCESS } from "../Authentication/ActionType";
+import {
+  GET_USERS_ORDERS_REQUEST,
+  GET_USERS_ORDERS_SUCCESS,
+} from "./ActionTypes";
+
+const initialState = {
+  loading: false,
+  orders: [],
+  error: null,
+};
+
+export const orderReducer = (state = initialState, { type, payload }) => {
+  switch (type) {
+    case GET_USERS_ORDERS_REQUEST:
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case GET_USERS_ORDERS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        orders: payload,
+        error: null,
+      };
+    case GET_USERS_ORDERS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: payload,
+      };
+
+    default:
+      return state;
+  }
+};
