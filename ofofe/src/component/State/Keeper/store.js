7be56ca09e3c1532404e323958b532f83d5cd2dd
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-15 13:52:31 +0100

    Store

--- ofofe/src/component/State/Keeper/store.js
+++ ofofe/src/component/State/Keeper/store.js
@@ -2,10 +2,12 @@ import { applyMiddleware, combineReducers, legacy_createStore } from "redux";
 import { authReducer } from "../Authentication/Reducer";
 import { thunk } from "redux-thunk";
 import menuItemsReducer from "../Menu/Reducer";
+import cartReducer from "../Cart/Reducer";
 
 const rooteReducer = combineReducers({
   auth: authReducer,
   restaurant: restaurantReducer,
   menu: menuItemsReducer,
+  cart: cartReducer,
 });
 export const store = legacy_createStore(rooteReducer, applyMiddleware(thunk));
