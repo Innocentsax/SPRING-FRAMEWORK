918be70efbe4b1f30efac02058a14de966e7ad21
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-16 08:32:51 +0100

    Restaurant Order

--- ofofe/src/component/State/Restaurant Oder/Action.js
+++ ofofe/src/component/State/Restaurant Oder/Action.js
@@ -1,7 +1,55 @@
-export const GET_RESTAURANTS_ORDER_REQUEST = "GET_RESTAURANTS_ORDER_REQUEST";
-export const GET_RESTAURANTS_ORDER_SUCCESS = "GET_RESTAURANTS_ORDER_SUCCESS";
-export const GET_RESTAURANTS_ORDER_FAILURE = "GET_RESTAURANTS_ORDER_FAILURE";
+import { api } from "../../Config/api";
+import {
+  GET_RESTAURANTS_ORDER_FAILURE,
+  GET_RESTAURANTS_ORDER_REQUEST,
+  GET_RESTAURANTS_ORDER_SUCCESS,
+  UPDATE_ORDER_STATUS_FAILURE,
+  UPDATE_ORDER_STATUS_REQUEST,
+  UPDATE_ORDER_STATUS_SUCCESS,
+} from "./ActionTypes";
 
-export const UPDATE_ORDER_STATUS_SUCCESS = "UPDATE_ORDER_STATUS_SUCCESS";
-export const UPDATE_ORDER_STATUS_FAILURE = "UPDATE_ORDER_STATUS_FAILURE";
-export const UPDATE_ORDER_STATUS_REQUEST = "UPDATE_ORDER_STATUS_REQUEST";
+export const updateOrderStatus = ({ orderId, orderStatus, jwt }) => {
+  return async (dispatch) => {
+    dispatch({ type: UPDATE_ORDER_STATUS_REQUEST });
+    try {
+      const response = await api.put(
+        `/api/admin/orders/${orderId}/${orderStatus}`,
+        {},
+        {
+          headers: { Authorization: `Bearer ${jwt}` },
+        }
+      );
+      const updatedOrder = response.data;
+      console.log("updated order ", updatedOrder);
+      dispatch({ type: UPDATE_ORDER_STATUS_SUCCESS, payload: updatedOrder });
+    } catch (error) {
+      console.log("catch error ", error);
+      dispatch({ type: UPDATE_ORDER_STATUS_FAILURE, payload: error });
+    }
+  };
+};
+
+export const fetchRestaurantsOrder = ({ restaurantId, orderStatus, jwt }) => {
+  return async (dispatch) => {
+    dispatch({ type: GET_RESTAURANTS_ORDER_REQUEST });
+    try {
+      const { data } = await api.get(
+        `/api/admin/order/restaurant/${restaurantId}`,
+        {
+          params: {
+            order_Status: orderStatus,
+          },
+        },
+        {
+          headers: { Authorization: `Bearer ${jwt}` },
+        }
+      );
+      const orders = data;
+      console.log("data ------ ", orders);
+      dispatch({ type: GET_RESTAURANTS_ORDER_SUCCESS, payload: orders });
+    } catch (error) {
+      console.log("error", error);
+      dispatch({ type: GET_RESTAURANTS_ORDER_FAILURE, payload: error });
+    }
+  };
+};
