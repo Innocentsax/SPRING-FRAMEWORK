dea77a472da8c2f385a5afab00840fe03e4ff843
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-17 09:20:32 +0100

    Reducer

--- ofofe/src/component/State/Authentication/Reducer.js
+++ ofofe/src/component/State/Authentication/Reducer.js
@@ -51,6 +51,7 @@ export const authReducer = (state = initialState, action) => {
         ...state,
         isLoading: false,
         user: action.payload,
+        favorites: action.payload.favorites,
       };
 
     case ADD_TO_FAVORITE_SUCCESS:
