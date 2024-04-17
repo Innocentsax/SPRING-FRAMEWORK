a8ac96a8944d03a91066f05cf37f88e84589992c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-17 09:19:37 +0100

    Home

--- ofofe/src/component/Home/Home.jsx
+++ ofofe/src/component/Home/Home.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./Home.css";
 import MultiItemCarousel from "./MultiItemCarousel";
 import RestaurantCard from "../Restaurant/RestaurantCard";
+import { useDispatch, useSelector } from "react-redux";
+import { getAllRestaurantsAction } from "../State/Restaurant/Action";
 
-const restaurant = [1, 1, 1, 1, 1, 1, 1, 1];
-function Home() {
+const Home = () => {
+  const dispatch = useDispatch();
+  const jwt = localStorage.getItem("jwt");
+  const { restaurant } = useSelector((store) => store);
+  console.log("restaurants", restaurant);
+  useEffect(() => {
+    dispatch(getAllRestaurantsAction(jwt));
+  }, []);
+
   return (
     <div className="pb-10">
       <section className="banner -z-50 relative flex flex-col justify-center items-center">
@@ -30,14 +39,14 @@ function Home() {
           Order From Handpicked Favorites
         </h1>
         <div className="flex flex-wrap items-center justify-round gap-5">
-          {restaurant.map((item) => (
-            <RestaurantCard />
+          {restaurant.restaurants.map((item) => (
+            <RestaurantCard item={item} />
           ))}
         </div>
         <div></div>
       </section>
     </div>
   );
-}
+};
 
 export default Home;
