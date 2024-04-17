a3cd52bcb01eb98925aea90262ceb038ad403d8b
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-17 09:20:03 +0100

    Restaurant

--- ofofe/src/component/Restaurant/RestaurantCard.jsx
+++ ofofe/src/component/Restaurant/RestaurantCard.jsx
@@ -2,8 +2,26 @@ import { Card, Chip, IconButton } from "@mui/material";
 import React from "react";
 import FavoriteIcon from "@mui/icons-material/Favorite";
 import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import { addToFavorite } from "../State/Authentication/Action";
+import { isPresentInFavorites } from "../Config/logic";
 
-const RestaurantCard = () => {
+const RestaurantCard = ({ item }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const jwt = localStorage.getItem("jwt");
+  const { auth } = useSelector((store) => store);
+
+  const handleAddToFavorite = () => {
+    dispatch(addToFavorite({ restaurantId: item.id, jwt }));
+  };
+
+  const handleNavigateToRestaurant = () => {
+    if (item.open) {
+      navigate(`/restaurant/${item.address.city}/${item.name}/${item.id}`);
+    }
+  };
   return (
     <Card className="w-[18rem]">
       <div
@@ -11,26 +29,30 @@ const RestaurantCard = () => {
       >
         <img
           className="w-full h-[10rem] rounded-t-md object-cover"
-          src="https://images.pexels.com/photos/460537/pexels-photo-460537.jpeg?auto=compress&cs=tinysrgb&w=800"
+          src={item.images[1]}
+          // src="https://images.pexels.com/photos/460537/pexels-photo-460537.jpeg?auto=compress&cs=tinysrgb&w=800"
           alt=""
         />
         <Chip
           size="small"
           className="absolute top-2 left-2"
-          color={true ? "success" : "error"}
-          label={true ? "open" : "closed"}
+          color={item.open ? "success" : "error"}
+          label={item.open ? "open" : "closed"}
         />
       </div>
       <div className="p-4 textPart lg:flex w-full justify-between">
         <div className="space-y-1">
-          <p className="font-semibold text-lg">TransCorp Hotel</p>
-          <p className="text-grey-500 text-sm">
-            Craving it all? Dive into our global 5 Star...
+          <p
+            onClick={handleNavigateToRestaurant}
+            className="font-semibold text-lg cursor-pointer"
+          >
+            {item.name}
           </p>
+          <p className="text-grey-500 text-sm">{item.description}</p>
         </div>
         <div>
-          <IconButton>
-            {true ? (
+          <IconButton onClick={handleAddToFavorite}>
+            {isPresentInFavorites(auth.favorites, item) ? (
               <FavoriteIcon />
             ) : (
               <FavoriteBorderIcon></FavoriteBorderIcon>
