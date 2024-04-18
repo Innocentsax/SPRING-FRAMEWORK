9a04b75dd53dd40c077145ad8be5501f0500148c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-18 01:56:35 +0100

    Ft | hot fix

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/controller/CategoryController.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/controller/CategoryController.java
@@ -29,10 +29,11 @@ public class CategoryController {
         return new ResponseEntity<>(createCategory, HttpStatus.CREATED);
     }
 
-    @GetMapping("/category/restaurant")
-    public ResponseEntity<List<Category>> getRestaurantCategory(@RequestHeader("Authorization") String jwt) throws Exception {
+    @GetMapping("/category/restaurant/{id}")
+    public ResponseEntity<List<Category>> getRestaurantCategory(@PathVariable Long id,
+            @RequestHeader("Authorization") String jwt) throws Exception {
         User user = userService.findUserByJwtToken(jwt);
-        List<Category> categories = categoryService.findCategoryByRestaurantId(user.getId());
+        List<Category> categories = categoryService.findCategoryByRestaurantId(id);
 
         return new ResponseEntity<>(categories, HttpStatus.CREATED);
     }
