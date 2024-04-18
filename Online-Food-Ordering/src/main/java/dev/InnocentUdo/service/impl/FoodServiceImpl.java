e75e385e3547fccaaf5a2ea7b000963b4304a136
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-18 01:56:48 +0100

    Ft | hot fix

--- Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/FoodServiceImpl.java
+++ Online-Food-Ordering/src/main/java/dev/InnocentUdo/service/impl/FoodServiceImpl.java
@@ -9,6 +9,7 @@ import dev.InnocentUdo.service.FoodService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -29,6 +30,7 @@ public class FoodServiceImpl implements FoodService {
         food.setIngredients(req.getIngredients());
         food.setSeasonal(req.isSeasonal());
         food.setVegetarian(req.isVegetarian());
+        food.setCreationDate(new Date());
 
         Food savedFood = foodRepository.save(food);
         restaurant.getFood().add(savedFood);
