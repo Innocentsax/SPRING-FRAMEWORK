c5a99e14a2617b7585cf4b5922edf1e388e793b9
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-21 14:47:21 +0100

    fix

--- ofofe/src/component/Cart/CartItem.jsx
+++ ofofe/src/component/Cart/CartItem.jsx
@@ -2,41 +2,60 @@ import React from "react";
 import RemoveCircleOutlineIcon from "@mui/icons-material/RemoveCircleOutline";
 import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 import { Chip, IconButton } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { removeCartItem, updateCartItem } from "../State/Cart/Action";
 
-const CartItem = () => {
+const CartItem = ({ item }) => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { auth, cart } = useSelector((store) => store);
+  const jwt = localStorage.getItem("jwt");
+
+  const handleUpdateCartItem = (value) => {
+    if (value === -1 && item.quantity === 1) {
+      handleRemoveItemFromCart();
+    }
+    const data = { cartItemId: item.id, quantity: item.quantity + value };
+    dispatch(updateCartItem(data, jwt));
+  };
+  const handleRemoveItemFromCart = () => {
+    dispatch(removeCartItem({ cartItemId: item.id, jwt: auth.jwt || jwt }));
+  };
+
   return (
     <div className="px5">
       <div className="lg:flex items-center lg:space-x-5">
         <div>
           <img
             className="px-5 w-[8rem] h-[5rem] object-cover"
-            src="https://cdn.pixabay.com/photo/2018/01/22/16/28/pancake-3099315_1280.jpg"
+            src={item.food.images[0]}
             alt=""
           />
         </div>
         <div className="flex items-center justify-between lg:w-[70%]">
           <div className="space-y-1 lg:space-y-3 w-full">
-            <p>Pancake</p>
+            <p>{item.food.name}</p>
             <div className="flex justify-between items-center">
               <div className="flex items-center space-x-1">
-                <IconButton>
+                <IconButton onClick={() => handleUpdateCartItem(-1)}>
                   <RemoveCircleOutlineIcon />
                 </IconButton>
                 <div className="w-5 h-5 text-xs flex items-center justify-center">
-                  {5}
+                  {item.quantity}
                 </div>
-                <IconButton>
+                <IconButton onClick={() => handleUpdateCartItem(1)}>
                   <AddCircleOutlineIcon />
                 </IconButton>
               </div>
             </div>
           </div>
-          <p>₦1,000</p>
+          <p>₦{item.totalPrice}</p>
         </div>
       </div>
       <div className="pt-3 space-x-2 px-5">
-        {[1, 1, 1].map((item) => (
-          <Chip label={"bread"} />
+        {item.ingredients.map((ingredient) => (
+          <Chip label={ingredient} />
         ))}
       </div>
     </div>
