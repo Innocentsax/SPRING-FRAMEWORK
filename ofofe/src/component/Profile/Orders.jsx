0f3f877e498ee2e04f013fa9ef09b3973458e2ad
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-21 15:26:34 +0100

    fix

--- ofofe/src/component/Profile/Orders.jsx
+++ ofofe/src/component/Profile/Orders.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import OrderCard from "./OrderCard";
+import { useSelector, useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getUsersOrders } from "../State/Order/Action";
 
 const Orders = () => {
+  const { cart, auth, order } = useSelector((store) => store);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const jwt = localStorage.getItem("jwt");
+
+  useEffect(() => {
+    dispatch(getUsersOrders(jwt));
+  }, [auth.jwt]);
+
   return (
     <div className="flex items-center flex-col">
       <h1 className="text-xl text-center py-7 font-semibold">My Order</h1>
       <div className="space-y-5 w-full lg:w-1/2">
-        {[1, 1, 1, 1].map((item) => (
-          <OrderCard />
-        ))}
+        {order.orders.map((order) =>
+          order.items.map((item) => <OrderCard order={order} item={item} />)
+        )}
       </div>
     </div>
   );
