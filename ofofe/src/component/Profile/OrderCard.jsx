8c0d1ebd15390aa4f9f3e80dd544f9f2ffc05be6
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-21 15:26:28 +0100

    fix

--- ofofe/src/component/Profile/OrderCard.jsx
+++ ofofe/src/component/Profile/OrderCard.jsx
@@ -1,22 +1,18 @@
 import { Button, Card } from "@mui/material";
 import React from "react";
 
-const OrderCard = () => {
+const OrderCard = (item, order) => {
   return (
     <Card className="flex justify-between items-center p-5">
       <div className="flex items-center space-x-5">
-        <img
-          className="h-16 w-16"
-          src="https://cdn.pixabay.com/photo/2018/07/10/21/23/pancake-3529653_1280.jpg"
-          alt=""
-        />
+        <img className="h-16 w-16" src={item.food.images[0]} alt="" />
         <div>
-          <p>Pancake</p>
-          <p>₦2000</p>
+          <p>{item.food.name}</p>
+          <p>₦{item.totalPrice}</p>
         </div>
       </div>
       <div>
-        <Button className="cursor-not-allow">Completed</Button>
+        <Button className="cursor-not-allow">{order.orderStatus}</Button>
       </div>
     </Card>
   );
