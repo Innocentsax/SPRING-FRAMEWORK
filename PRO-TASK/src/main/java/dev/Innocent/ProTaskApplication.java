49c59739b5a08ad885f4a5322d493e1ffb9570fd
Innocent Udo <innocentcharlesudo@gmail.com>
2024-04-28 18:50:30 +0100

    Tasks Management

---
+++ PRO-TASK/src/main/java/dev/Innocent/ProTaskApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProTaskApplication.class, args);
+	}
+
+}
