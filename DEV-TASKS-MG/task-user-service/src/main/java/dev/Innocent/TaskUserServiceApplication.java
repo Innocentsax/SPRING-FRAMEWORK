68b4e38f8bd4942000ac714b4e9c9221c8f6fabe
Innocent Udo <innocentcharlesudo@gmail.com>
2024-04-29 09:44:17 +0100

    User service setup

---
+++ DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/TaskUserServiceApplication.java
@@ -0,0 +1,13 @@
+package dev.Innocent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskUserServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskUserServiceApplication.class, args);
+	}
+
+}
