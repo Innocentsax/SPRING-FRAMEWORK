7207ff53610f771e7a68d4e6431e40be9352cdce
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-30 02:19:19 +0100

    Auth

---
+++ DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/DTO/request/LoginRequest.java
@@ -0,0 +1,13 @@
+package dev.Innocent.DTO.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginRequest {
+    private String email;
+    private String password;
+}
