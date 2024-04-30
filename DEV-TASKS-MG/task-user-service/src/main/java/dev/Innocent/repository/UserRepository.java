836c62f396b93cdf166957eff9163e2c79979fb4
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-30 02:20:01 +0100

    Auth

---
+++ DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package dev.Innocent.repository;
+
+import dev.Innocent.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    public User findByEmail(String email);
+}
