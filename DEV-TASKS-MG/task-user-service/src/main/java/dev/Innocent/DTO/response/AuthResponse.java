96abde97941580afe71e3b3a942d3c8bf54298c0
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-30 02:17:26 +0100

    Auth

---
+++ DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/DTO/response/AuthResponse.java
@@ -0,0 +1,14 @@
+package dev.Innocent.DTO.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    private String jwt;
+    private String message;
+    private boolean status;
+}
---
+++ DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/controller/AuthController.java
@@ -0,0 +1,90 @@
+package dev.Innocent.controller;
+
+import dev.Innocent.Config.JwtProvider;
+import dev.Innocent.DTO.request.LoginRequest;
+import dev.Innocent.DTO.response.AuthResponse;
+import dev.Innocent.model.User;
+import dev.Innocent.repository.UserRepository;
+import dev.Innocent.service.Impl.CustomServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtProvider jwtProvider;
+    private final CustomServiceImpl customService;
+
+    @PostMapping("/signup")
+    public ResponseEntity<AuthResponse> createUserHandler(@RequestBody User user) throws Exception {
+        User isEmailExist  = userRepository.findByEmail(user.getEmail());
+        if(isEmailExist != null){
+            throw new Exception("Email is already used with another account");
+        }
+        User createUser = new User();
+        createUser.setEmail(user.getEmail());
+        createUser.setFullName(user.getFullName());
+        createUser.setRole(user.getRole());
+        createUser.setPassword(passwordEncoder.encode(user.getPassword()));
+        userRepository.save(createUser);
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(),user.getPassword());
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        String jwt = jwtProvider.generateToken(authentication);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Register success");
+        authResponse.setStatus(true);
+
+        return new ResponseEntity<>(authResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/sign-in")
+    public ResponseEntity<AuthResponse> signIn(@RequestBody LoginRequest req){
+        String username = req.getEmail();
+        String password = req.getPassword();
+
+        Authentication authentication = authenticate(username, password);
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String role = authorities.isEmpty() ? null : authorities.iterator().next().getAuthority();
+
+        String jwt = jwtProvider.generateToken(authentication);
+        AuthResponse authResponse = new AuthResponse();
+        authResponse.setJwt(jwt);
+        authResponse.setMessage("Login success");
+        authResponse.setStatus(true);
+
+        return new ResponseEntity<>(authResponse, HttpStatus.OK);
+
+    }
+
+    private Authentication authenticate(String username, String password) {
+        UserDetails userDetails = customService.loadUserByUsername(username);
+
+        if(userDetails == null){
+            throw new BadCredentialsException("Invalid username...");
+        }
+        if(!passwordEncoder.matches(password,userDetails.getPassword())){
+            throw new BadCredentialsException("Invalid password....");
+        }
+        return new UsernamePasswordAuthenticationToken(userDetails, null,userDetails.getAuthorities());
+    }
+}
