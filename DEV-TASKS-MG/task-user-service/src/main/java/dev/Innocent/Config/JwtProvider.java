7c7e382dcec4fea87a1ba807056af37c6d47ba72
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-30 16:26:47 +0100

    Auth

--- DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/Config/JwtProvider.java
+++ DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/Config/JwtProvider.java
@@ -15,25 +15,28 @@ import java.util.Set;
 
 @Service
 public class JwtProvider {
-    private SecretKey key = Keys.hmacShaKeyFor(JwtConstants.SECRET_KEY.getBytes());
+    private final SecretKey key = Keys.hmacShaKeyFor(JwtConstants.SECRET_KEY.getBytes());
 
     public String generateToken(Authentication auth){
         Collection<? extends GrantedAuthority> authorities = auth.getAuthorities();
         String roles = populateAuthorities(authorities);
 
-        return Jwts.builder().setIssuedAt(new Date())
+        String jwt = Jwts.builder().setIssuedAt(new Date())
                 .setExpiration(new Date(new Date().getTime() + 86400000))
                 .claim("email",auth.getName())
                 .claim("authorities",roles)
                 .signWith(key)
                 .compact();
+
+        return jwt;
     }
 
     public String getEmailFromJwtToken(String jwt){
         jwt = jwt.substring(7);
         Claims claims = Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
 
-        return String.valueOf(claims.get("email"));
+        String email = String.valueOf(claims.get("email"));
+        return email;
     }
 
     private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
