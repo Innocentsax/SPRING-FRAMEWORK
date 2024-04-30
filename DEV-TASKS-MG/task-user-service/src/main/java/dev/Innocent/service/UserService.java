1a37f10240700f0675416c86c75c302e0dc1be9a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-30 17:10:37 +0100

    User Profile

--- DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/service/UserService.java
+++ DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/service/UserService.java
@@ -1,4 +1,10 @@
 package dev.Innocent.service;
 
+import dev.Innocent.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    public User getUserProfile(String jwt);
+    public List<User> getAllUsers();
 }
