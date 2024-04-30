7eaa630ed55ef0286b4e2adf1589078ac158ceec
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-30 16:26:43 +0100

    Auth

--- DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/controller/AuthController.java
+++ DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/controller/AuthController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping("/auths")
 @RequiredArgsConstructor
 public class AuthController {
     private final UserRepository userRepository;
