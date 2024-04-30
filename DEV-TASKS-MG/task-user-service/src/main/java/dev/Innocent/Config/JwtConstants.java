ce76b224fecc2c4710c5f57b31fa610163be780a
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-04-30 02:18:08 +0100

    Auth

---
+++ DEV-TASKS-MG/task-user-service/src/main/java/dev/Innocent/Config/JwtConstants.java
@@ -0,0 +1,6 @@
+package dev.Innocent.Config;
+
+public class JwtConstants {
+    public static final String SECRET_KEY = "jdfnekjdbnfxknvlbljfmsbnfjfjbslfnsjfblwngjrbvkjfhdbvkdjfksnjllflnglsjlrbgjsrfkjdsflkjnjnfbfklj";
+    public static final String JWT_HEADER = "Authorization";
+}
