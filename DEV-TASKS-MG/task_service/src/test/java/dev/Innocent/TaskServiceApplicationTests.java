008b498ae376e4c65149dc203a746ebdecda88cb
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-01 13:39:44 +0100

    Task Service Setup

---
+++ DEV-TASKS-MG/task_service/src/test/java/dev/Innocent/TaskServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.Innocent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
