ac783ea26fb5e4802a20a4dfc7bf469b180d6a5c
INNOCENT UDO <innocentcharlesudo@gmail.com>
2024-05-01 19:53:20 +0100

    Task Service Setup

--- DEV-TASKS-MG/task_service/src/main/java/dev/Innocent/TaskServiceApplication.java
+++ DEV-TASKS-MG/task_service/src/main/java/dev/Innocent/TaskServiceApplication.java
@@ -2,12 +2,13 @@ package dev.Innocent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class TaskServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskServiceApplication.class, args);
 	}
-
 }
